660fefe22165257f95b5fbf341fca2de25640a36
Darren Shepherd <darren.s.shepherd@gmail.com>
2013-11-13 11:45:47 -0700

    Changes

--- resources/content/db/mysql/create_db_and_user_dev.sql
+++ resources/content/db/mysql/create_db_and_user_dev.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE cloud COLLATE = 'utf8_general_ci' CHARACTER SET = 'utf8';
+CREATE DATABASE dstack COLLATE = 'utf8_general_ci' CHARACTER SET = 'utf8';
 CREATE USER 'dstack'@'%' IDENTIFIED BY 'dstack';
 CREATE USER 'dstack'@'localhost' IDENTIFIED BY 'dstack';
-GRANT ALL ON cloud.* TO 'dstack'@'%';
-GRANT ALL ON cloud.* TO 'dstack'@'localhost';
+GRANT ALL ON dstack.* TO 'dstack'@'%';
+GRANT ALL ON dstack.* TO 'dstack'@'localhost';
