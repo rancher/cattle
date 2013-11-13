77e755be03bc61511f19053bff30c661c33d3b5b
Darren Shepherd <darren.s.shepherd@gmail.com>
2013-11-13 11:37:20 -0700

    Jooq Changes

--- jooq/codegen.sh
+++ jooq/codegen.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+cd $(dirname $0)
+pushd ..
+mvn -am -pl jooq install
+popd
 mvn -Dexec.classpathScope=test -Dexec.mainClass=org.jooq.util.GenerationTool -Dexec.arguments="/codegen.xml" package exec:java
