fcea3f7895a5a2bd4cc41fadede86b33c0a673ec
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-01-03 17:22:35 -0700

    DB content

--- resources/content/db/data.sql
+++ resources/content/db/data.sql
@@ -2,5 +2,6 @@
 
 --changeset darren:sql1
 
-INSERT INTO account(id, uuid, kind, name, state) VALUES(1, UUID(), 'admin', 'admin', 'active');
+INSERT INTO account(id, uuid, kind, name, state) VALUES(1, 'admin', 'admin', 'admin', 'active');
+INSERT INTO account(id, uuid, kind, name, state) VALUES(2, 'system', 'system', 'system', 'active');
 INSERT INTO zone(id, uuid, name, state) VALUES(1, UUID(), 'zone1', 'active');
