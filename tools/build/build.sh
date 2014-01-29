781618f4493f2918e315fdd2f7775a6e91fecf42
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-01-29 08:43:55 -0700

    Fix build

--- tools/build/build.sh
+++ tools/build/build.sh
@@ -3,6 +3,6 @@ set -e
 
 cd $(dirname $0)/../..
 
-mvn -T1.5C -B -DskipTests -Dnot-iaas -Dnot-implementation install
-mvn -T1.5C -B -DskipTests -P!gdapi-java-server,!jedis,!jooq -Dnot-implementation install
-mvn -T1.5C -B -P!gdapi-java-server,!jedis,!jooq install
+mvn -B -DskipTests -Dnot-iaas -Dnot-implementation install
+mvn -B -DskipTests -P!gdapi-java-server,!jedis,!jooq -Dnot-implementation install
+mvn -B -P!gdapi-java-server,!jedis,!jooq install
