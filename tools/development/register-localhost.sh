f79e60c9d256ff2eef155251118e0b979dd2293e
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-03-18 13:42:22 -0700

    Add whitespace

--- tools/development/register-localhost.sh
+++ tools/development/register-localhost.sh
@@ -5,6 +5,7 @@ set -e
 docker info
 
 curl -s http://localhost:8080/v1/authorized_keys | sudo tee -a /root/.ssh/authorized_keys
+
 # Setting the uuid=test-agent is to make this not conflict with the integration tests.
 # This was the automated test will use the same agent and not try to create a second one.
 curl -X POST http://localhost:8080/v1/agents -F uuid=test-agent
