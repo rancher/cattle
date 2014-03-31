1576b72a15adb9c2034d5d5f097e6578b00de716
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-03-31 16:34:53 -0700

    Log aws-cf install to /var/log/cattle-install.log

--- docs/scripts/cf-install.sh
+++ docs/scripts/cf-install.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-# Install docker if you don't have it
-[ ! -x "$(which docker)" ] && curl -sL https://get.docker.io/ | sh
+do_install()
+{
+    # Install docker if you don't have it
+    [ ! -x "$(which docker)" ] && curl -sL https://get.docker.io/ | sh
 
-# Install libvirt too
-sudo apt-get install -y libvirt-bin python-libvirt python-pip
+    # Install libvirt too
+    sudo apt-get install -y libvirt-bin python-libvirt python-pip
 
-# Gonna need a ssh server
-sudo apt-get install -y openssh-server
+    # Gonna need a ssh server
+    sudo apt-get install -y openssh-server
 
-# CLI
-pip install cattle
+    # CLI
+    pip install cattle
 
-# Start Cattle
-sudo docker run -d -p 8080:8080 cattle/server
+    # Start Cattle
+    sudo docker run -d -p 8080:8080 cattle/server
 
-# Download and authorize SSH key.
-while true; do
-        curl -s http://localhost:8080/v1/authorized_keys | sudo tee -a /root/.ssh/authorized_keys
-        if [ ${PIPESTATUS[0]} == 0 ]; then
-                break
-        else
-                echo Waiting
-                sleep 5
-        fi
-done
+    # Download and authorize SSH key.
+    while true; do
+            curl -s http://localhost:8080/v1/authorized_keys | sudo tee -a /root/.ssh/authorized_keys
+            if [ ${PIPESTATUS[0]} == 0 ]; then
+                    break
+            else
+                    echo Waiting
+                    sleep 5
+            fi
+    done
 
-# Register agent
-curl -X POST http://localhost:8080/v1/agents
+    # Register agent
+    curl -X POST http://localhost:8080/v1/agents
+}
+
+do_install > /var/log/cattle-install.log 2>&
