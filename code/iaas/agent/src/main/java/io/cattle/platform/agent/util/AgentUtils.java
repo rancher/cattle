4ebb3326db5f63a478b767bcfbbe4d8f38c52dbb
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-04-15 10:59:02 -0700

    Add convienence method for agentAuth

--- code/iaas/agent/src/main/java/io/cattle/platform/agent/util/AgentUtils.java
+++ code/iaas/agent/src/main/java/io/cattle/platform/agent/util/AgentUtils.java
@@ -1,8 +1,17 @@
 package io.cattle.platform.agent.util;
 
 import io.cattle.platform.core.constants.AgentConstants;
+import io.cattle.platform.core.constants.CommonStatesConstants;
+import io.cattle.platform.core.constants.CredentialConstants;
+import io.cattle.platform.core.model.Account;
 import io.cattle.platform.core.model.Agent;
+import io.cattle.platform.core.model.Credential;
 import io.cattle.platform.framework.event.Ping;
+import io.cattle.platform.object.ObjectManager;
+
+import java.io.UnsupportedEncodingException;
+
+import org.apache.commons.codec.binary.Base64;
 
 public class AgentUtils {
 
@@ -19,5 +28,23 @@ public class AgentUtils {
         ping.setResourceId(Long.toString(agentId));
 
         return ping;
+    }
+
+    public static String getAgentAuth(Agent agent, ObjectManager objectManager) {
+        Account account = objectManager.loadResource(Account.class, agent.getAccountId());
+        if ( account == null ) {
+            return null;
+        }
+
+        for ( Credential cred : objectManager.children(account, Credential.class) ) {
+            if ( CredentialConstants.KIND_API_KEY.equals(cred.getKind()) && CommonStatesConstants.ACTIVE.equals(cred.getState()) ) {
+                try {
+                    return "Basic " + Base64.encodeBase64String((cred.getPublicValue() + ":" + cred.getSecretValue()).getBytes("UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                }
+            }
+        }
+
+        return null;
     }
 }
