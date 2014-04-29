f08a54e9e8f0885e1b1f90d571a2cf45cc9e8b1f
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-04-28 22:18:40 -0700

    [agent] binary deps for pyagent

---
+++ code/agent/src/agents/pyagent/binary-deps/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /util-linux*
+make install
+
+cd /socat*
+make install
--- code/agent/src/agents/pyagent/package.sh
+++ code/agent/src/agents/pyagent/package.sh
@@ -1,25 +1,69 @@
 #!/bin/bash
+set -e
 
 cd $(dirname $0)
 
-if diff -q dist-requirements.txt dist/dist-requirements.txt >/dev/null 2>&1; then
-    exit 0
-fi
+python_deps()
+{
+    if diff -q dist-requirements.txt dist/dist-requirements.txt >/dev/null 2>&1; then
+        return 0
+    fi
 
-VER=$(pip --version | awk '{print $2}')
-MAJOR=$(echo $VER | cut -f1 -d.)
-MINOR=$(echo $VER | cut -f2 -d.)
-if [ $MAJOR -lt 2 ] && [ $MINOR -lt 5 ]
-then
-    echo "[ERROR] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 1>&2
-    echo "[ERROR] !! pip 1.5 or newer is required !!" 1>&2
-    echo "[ERROR] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 1>&2
-    exit 1
-fi
+    VER=$(pip --version | awk '{print $2}')
+    MAJOR=$(echo $VER | cut -f1 -d.)
+    MINOR=$(echo $VER | cut -f2 -d.)
+    if [ $MAJOR -lt 2 ] && [ $MINOR -lt 5 ]
+    then
+        echo "[ERROR] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 1>&2
+        echo "[ERROR] !! pip 1.5 or newer is required !!" 1>&2
+        echo "[ERROR] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 1>&2
+        exit 1
+    fi
 
-if [ -e dist ]; then
-    rm -rf dist
-fi
+    if [ -e dist ]; then
+        rm -rf dist
+    fi
 
-pip install -t dist -r dist-requirements.txt
-cp dist-requirements.txt dist
+    pip install -t dist -r dist-requirements.txt
+    cp dist-requirements.txt dist
+}
+
+docker_build()
+{
+    cd binary-deps
+    docker build -t agent-build .
+    ID=$(docker run -d agent-build echo)
+    docker cp ${ID}:/usr/bin/nsenter ../cattle/plugins/docker || true
+    docker cp ${ID}:/usr/local/bin/socat ../cattle/plugins/core || true
+}
+
+local_build()
+{
+    if [ -e build ]; then
+        rm -rf build
+    fi
+
+    mkdir -p build
+    cd build
+
+    ../binary-deps/build.sh
+
+    cp util-linux*/nsenter ../cattle/plugins/docker/nsenter
+    cp socat*/socat ../cattle/plugins/core/socat
+}
+
+binary_deps()
+{
+    if [[ -e ./cattle/plugins/core/socat && -e ./cattle/plugins/docker/nsenter ]]; then
+        return 0
+    fi
+
+    if docker info >/dev/null 2>&1; then
+        docker_build
+    else
+        local_build
+    fi
+}
+
+python_deps
+binary_deps
