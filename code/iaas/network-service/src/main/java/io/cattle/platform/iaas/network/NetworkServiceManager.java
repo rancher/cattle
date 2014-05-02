522fd7f755da61849380873df8f213bc1593a748
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-05-02 14:47:59 -0700

    [packaging] delete network service

--- code/iaas/network-service/src/main/java/io/cattle/platform/iaas/network/NetworkServiceManager.java
+++
@@ -1,9 +0,0 @@
-package io.cattle.platform.iaas.network;
-
-import io.cattle.platform.core.model.NetworkService;
-
-public interface NetworkServiceManager {
-
-    NetworkService getService(Long networkId, String networkService);
-
-}
--- code/iaas/network-service/src/main/java/io/cattle/platform/iaas/network/dao/NetworkServiceDao.java
+++
@@ -1,9 +0,0 @@
-package io.cattle.platform.iaas.network.dao;
-
-import io.cattle.platform.core.model.NetworkService;
-
-public interface NetworkServiceDao {
-
-    NetworkService getService(Long networkId, String networkService);
-
-}
