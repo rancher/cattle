e989e0c295986f75296fab16a3c59af5bd781764
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-05-05 10:42:53 -0700

    add api key

---
+++ tests/integration/cattletest/core/test_apikey.py
@@ -0,0 +1,13 @@
+from common_fixtures import *  # NOQA
+
+
+def test_api_key_create(client):
+    assert_required_fields(client.create_api_key)
+
+    key = client.create_api_key()
+    assert key.state == 'registering'
+
+    key = client.wait_transitioning(key)
+    assert key.state == 'active'
+    assert key.publicValue is not None
+    assert key.secretValue is not None
