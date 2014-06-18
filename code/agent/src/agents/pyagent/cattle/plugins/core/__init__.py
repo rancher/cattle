c93f22e57fe2dd97c7956bb2098066d95f64fc87
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-06-18 13:52:04 -0700

    [pyagent] Don't make config handler optional anymore

--- code/agent/src/agents/pyagent/cattle/plugins/core/__init__.py
+++ code/agent/src/agents/pyagent/cattle/plugins/core/__init__.py
@@ -9,6 +9,5 @@ from cattle.type_manager import POST_REQUEST_HANDLER, LIFECYCLE
 register_type(MARSHALLER, marshaller.Marshaller())
 register_type(ROUTER, event_router.Router())
 register_type(POST_REQUEST_HANDLER, event_handlers.PingHandler())
-if Config.config_update():
-    register_type(POST_REQUEST_HANDLER, event_handlers.ConfigUpdateHandler())
+register_type(POST_REQUEST_HANDLER, event_handlers.ConfigUpdateHandler())
 register_type(LIFECYCLE, api_proxy.ApiProxy())
