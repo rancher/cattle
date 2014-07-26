491dba3f6851c413d35a8c5275f8babed8a06bbf
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-07-26 08:25:47 -0700

    package: genisoimage is required for config-drive support

--- docs/scripts/cf-install.sh
+++ docs/scripts/cf-install.sh
@@ -6,7 +6,7 @@ do_install()
     [ ! -x "$(which docker)" ] && curl -sL https://get.docker.io/ | sh
 
     # Install libvirt too
-    sudo apt-get install -y libvirt-bin python-libvirt qemu-kvm python-pip python-numpy arptables
+    sudo apt-get install -y libvirt-bin python-libvirt qemu-kvm python-pip python-numpy arptables genisoimage
 
     # Gonna need a ssh server
     sudo apt-get install -y openssh-server
