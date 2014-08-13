de755b5291d6c63b2d6283fc4b63db1c9a17dd3e
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-08-13 11:35:45 -0700

    config-config: if ipsec-hosts runs before ipsec it will fail

--- resources/content/config-content/ipsec-hosts/apply.sh
+++ resources/content/config-content/ipsec-hosts/apply.sh
@@ -2,9 +2,20 @@
 
 . ${CATTLE_HOME:-/var/lib/cattle}/common/scripts.sh
 
+ARPDEV=arpproxy
 IFACE=eth0
 IP=$(ifconfig $IFACE | grep 'inet addr:' | awk '{print $2}' | cut -f2 -d:)
 
+create_dummy()
+{
+    if ! ip link show dev $ARPDEV >/dev/null 2>&1; then
+        ip link add dev $ARPDEV type dummy
+    fi
+
+    ip link set dev $ARPDEV up
+}
+
+create_dummy
 add_route_table 200 "iif ${IFACE}"
 
 sed -i 's/%HOST_IP%/'$IP'/g' content-home/etc/cattle/setkey
