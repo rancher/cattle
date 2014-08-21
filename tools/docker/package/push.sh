d9f92c80471f0cdf28eacbc6860974932c06b133
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-08-21 08:56:16 -0700

    build: add arg to push as latest

--- tools/docker/package/push.sh
+++ tools/docker/package/push.sh
@@ -5,5 +5,10 @@ cd $(dirname $0)
 if [ -e images ]; then
     for i in $(<images); do
         docker push $i
+        if [ "$1" == "--latest" ] || [ "$1" == "-l" ]; then
+            LATEST_TAG="$(echo $i | sed 's/:.*/:latest')"
+            docker tag $i $LATEST_TAG
+            docker push $LATEST_TAG
+        fi
     done
 fi
