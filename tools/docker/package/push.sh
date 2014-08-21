09ac8a55b32f1913c50e522af0e56dee295fa35c
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-08-21 13:44:32 -0700

    build: fix bad sed command

--- tools/docker/package/push.sh
+++ tools/docker/package/push.sh
@@ -6,7 +6,7 @@ if [ -e images ]; then
     for i in $(<images); do
         docker push $i
         if [ "$1" == "--latest" ] || [ "$1" == "-l" ]; then
-            LATEST_TAG="$(echo $i | sed 's/:.*/:latest')"
+            LATEST_TAG="$(echo $i | sed 's/:.*/:latest/g')"
             docker tag $i $LATEST_TAG
             docker push $LATEST_TAG
         fi
