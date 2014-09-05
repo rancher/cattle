fd67c2e3bf2d4958a8039f7d8a1e265a705cb2c2
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-09-05 13:38:48 -0700

    docker: don't assume ports is set

--- code/agent/src/agents/pyagent/cattle/plugins/docker/network/links.py
+++ code/agent/src/agents/pyagent/cattle/plugins/docker/network/links.py
@@ -23,25 +23,29 @@ class LinkSetup(BaseHandler):
         result = {}
         for link in instance.instanceLinks:
             name = link.linkName
-            for link_port in link.data.fields.ports:
-                proto = link_port.protocol
-                ip = link_port.ipAddress
-                dst = link_port.publicPort
-                port = link_port.privatePort
 
-                full_port = '{0}://{1}:{2}'.format(proto, ip, dst)
+            try:
+                for link_port in link.data.fields.ports:
+                    proto = link_port.protocol
+                    ip = link_port.ipAddress
+                    dst = link_port.publicPort
+                    port = link_port.privatePort
 
-                data = {
-                    'NAME': '/cattle/{0}'.format(name),
-                    'PORT': full_port,
-                    'PORT_{0}_{1}'.format(port, proto): full_port,
-                    'PORT_{0}_{1}_ADDR'.format(port, proto): ip,
-                    'PORT_{0}_{1}_PORT'.format(port, proto): dst,
-                    'PORT_{0}_{1}_PROTO'.format(port, proto): proto,
-                }
+                    full_port = '{0}://{1}:{2}'.format(proto, ip, dst)
 
-                for k, v in data.items():
-                    result['{0}_{1}'.format(name, k).upper()] = v
+                    data = {
+                        'NAME': '/cattle/{0}'.format(name),
+                        'PORT': full_port,
+                        'PORT_{0}_{1}'.format(port, proto): full_port,
+                        'PORT_{0}_{1}_ADDR'.format(port, proto): ip,
+                        'PORT_{0}_{1}_PORT'.format(port, proto): dst,
+                        'PORT_{0}_{1}_PROTO'.format(port, proto): proto,
+                    }
+
+                    for k, v in data.items():
+                        result['{0}_{1}'.format(name, k).upper()] = v
+            except AttributeError:
+                pass
 
         if len(result) > 0:
             add_to_env(config, **result)
