96965966493df3c6100fe4188051b030104745ad
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-09-05 13:45:47 -0700

    docker: update kibana

--- tools/docker/kibana/run.sh
+++ tools/docker/kibana/run.sh
@@ -2,7 +2,7 @@
 set -e
 
 sed -i -e 's/window.location.hostname/"'${ES_PORT_9200_TCP_ADDR}'"/g' \
-       -e 's/9200/'${ES_PORT_9200_TCP_PORT}'/g' /var/www/config.js
+       -e 's/9200/'${ES_PORT_9200_TCP_PORT}'/g' /var/www/html/config.js
 
 /etc/init.d/apache2 start
 
