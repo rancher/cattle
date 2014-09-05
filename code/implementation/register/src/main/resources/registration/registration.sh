95781b7ecc029bd74b1d0c10902bcf84835bd5ce
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-09-05 13:42:36 -0700

    update registration script

--- code/implementation/register/src/main/resources/registration/registration.sh
+++ code/implementation/register/src/main/resources/registration/registration.sh
@@ -6,11 +6,16 @@ export CATTLE_URL="${CATTLE_URL}"
 
 if [ "$CATTLE_RUN_REGISTRATION" != "false" ]; then
     ID=$(docker run \
+        --name cattle-agent \
+        --privileged \
         -e CATTLE_REGISTRATION_ACCESS_KEY="${CATTLE_REGISTRATION_ACCESS_KEY}" \
         -e CATTLE_REGISTRATION_SECRET_KEY="${CATTLE_REGISTRATION_SECRET_KEY}" \
         -e CATTLE_AGENT_IP="${CATTLE_AGENT_IP}" \
         -e CATTLE_URL="${CATTLE_URL}" \
-        -v /var:/host/var \
+        -v /lib/modules:/host/lib/modules:ro \
+        -v /var/lib/docker:/host/var/lib/docker \
+        -v /var/lib/cattle:/host/var/lib/cattle \
+        -v /opt/bin:/host/opt/bin \
         -v /proc:/host/proc \
         -v /run:/host/run \
         -d \
