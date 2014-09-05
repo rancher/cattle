285105f0fbfdd071f8060d8607a17fed23ccec92
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-09-05 13:45:31 -0700

    dev: make register script URL configurable

--- tools/development/register-localhost.sh
+++ tools/development/register-localhost.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
+DEV_HOST=${DEV_HOST:-localhost:8080}
+
 # This is just here to make sure your environment is sane
 docker info
 
-curl -s http://localhost:8080/v1/authorized_keys | sudo tee -a /root/.ssh/authorized_keys
+curl -s http://${DEV_HOST}/v1/authorized_keys | sudo tee -a /root/.ssh/authorized_keys
 
 # Setting the uuid=test-agent is to make this not conflict with the integration tests.
 # This was the automated test will use the same agent and not try to create a second one.
-curl -X POST http://localhost:8080/v1/agents -F uuid=test-agent
+curl -X POST http://${DEV_HOST}/v1/agents -F uuid=test-agent
