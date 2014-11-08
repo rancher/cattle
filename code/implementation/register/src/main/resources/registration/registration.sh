ac26b2af78e1722fa621fdb0ca3051748b520d3b
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-11-07 20:15:31 -0700

    register: only export variables, don't run docker anymore

--- code/implementation/register/src/main/resources/registration/registration.sh
+++ code/implementation/register/src/main/resources/registration/registration.sh
@@ -3,23 +3,4 @@
 export CATTLE_REGISTRATION_ACCESS_KEY="${CATTLE_REGISTRATION_ACCESS_KEY}"
 export CATTLE_REGISTRATION_SECRET_KEY="${CATTLE_REGISTRATION_SECRET_KEY}"
 export CATTLE_URL="${CATTLE_URL}"
-
-if [ "$CATTLE_RUN_REGISTRATION" != "false" ]; then
-    ID=$(docker run \
-        --name cattle-agent \
-        --privileged \
-        -e CATTLE_REGISTRATION_ACCESS_KEY="${CATTLE_REGISTRATION_ACCESS_KEY}" \
-        -e CATTLE_REGISTRATION_SECRET_KEY="${CATTLE_REGISTRATION_SECRET_KEY}" \
-        -e CATTLE_AGENT_IP="${CATTLE_AGENT_IP}" \
-        -e CATTLE_URL="${CATTLE_URL}" \
-        -v /lib/modules:/host/lib/modules:ro \
-        -v /var/lib/docker:/host/var/lib/docker \
-        -v /var/lib/cattle:/host/var/lib/cattle \
-        -v /opt/bin:/host/opt/bin \
-        -v /proc:/host/proc \
-        -v /run:/host/run \
-        -d \
-        "${CATTLE_AGENT_IMAGE}")
-
-    echo "Launched container $ID, run \"docker logs -f $ID\" for progress"
-fi
+export DETECTED_CATTLE_AGENT_IP="${CATTLE_AGENT_IP}"
