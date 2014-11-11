e584b1579123fb430d97196a1ab5542f2059adef
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-11-11 00:52:08 -0700

    scripts: rename reload to drop_tables

--- resources/content/db/mysql/drop_tables.sh
+++ resources/content/db/mysql/drop_tables.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)
 
 TS=$(date '+%s')
 mysqldump -u root cattle > cattle_dump_${TS}.sql
-mysql -u root < reload_db.sql
+mysql -u root < drop_tables.sql
--- resources/content/db/mysql/drop_tables_base.sh
+++ resources/content/db/mysql/drop_tables_base.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)
 
 TS=$(date '+%s')
 mysqldump -u root cattle_base > cattle_dump_base_${TS}.sql
-mysql -u root < reload_db_base.sql
+mysql -u root < drop_tables_base.sql
