9f743c81edce39f80d96a56d4bc1b6a6b1bb13f5
Bill Maxwell <bill@rancher.com>
2014-11-12 21:37:40 -0800

    removing --rm for now until we figure out why it fails to remove on btrfs

--- tools/docker/build.sh
+++ tools/docker/build.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)/../..
 
-CMD="docker run --rm -e BUILD_USER_ID=$(id -u) -e CATTLE_DB_CATTLE_DATABASE=$FORCE_DB -v $(pwd):/root -w /root -t -i"
+CMD="docker run -e BUILD_USER_ID=$(id -u) -e CATTLE_DB_CATTLE_DATABASE=$FORCE_DB -v $(pwd):/root -w /root -t -i"
 
 if [ "$1" = "run" ]; then
     CMD="$CMD -p ${CATTLE_HTTP_PORT:-8080}:8080"
