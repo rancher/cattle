29ed71e6a1725e556ca4bc3c8cdf75a7eef6dbd6
sidharthamani <sidharthamn@gmail.com>
2015-01-05 15:58:20 -0700

    [Issue #70] Add token account lookup

--- code/iaas/api-logic/src/main/java/io/cattle/platform/iaas/api/auth/dao/AuthDao.java
+++ code/iaas/api-logic/src/main/java/io/cattle/platform/iaas/api/auth/dao/AuthDao.java
@@ -8,4 +8,10 @@ public interface AuthDao {
 
     Account getAccountByKeys(String access, String secretKey);
 
+    Account getAccountByExternalId(String externalId, String exteralType);
+    
+    Account getAccountByUuid(String uuid);
+    
+    Account createAccount(String name, String kind, String externalId, String externalType);
+    
 }
--- code/iaas/api-logic/src/main/java/io/cattle/platform/iaas/api/auth/dao/impl/AuthDaoImpl.java
+++ code/iaas/api-logic/src/main/java/io/cattle/platform/iaas/api/auth/dao/impl/AuthDaoImpl.java
@@ -1,21 +1,30 @@
 package io.cattle.platform.iaas.api.auth.dao.impl;
 
-import static io.cattle.platform.core.model.tables.CredentialTable.*;
-import static io.cattle.platform.core.model.tables.AccountTable.*;
-
-import com.netflix.config.DynamicStringListProperty;
-
+import static io.cattle.platform.core.model.tables.AccountTable.ACCOUNT;
+import static io.cattle.platform.core.model.tables.CredentialTable.CREDENTIAL;
 import io.cattle.platform.archaius.util.ArchaiusUtil;
 import io.cattle.platform.core.constants.CommonStatesConstants;
+import io.cattle.platform.core.dao.GenericResourceDao;
 import io.cattle.platform.core.model.Account;
 import io.cattle.platform.core.model.tables.records.AccountRecord;
 import io.cattle.platform.db.jooq.dao.impl.AbstractJooqDao;
 import io.cattle.platform.iaas.api.auth.dao.AuthDao;
+import io.cattle.platform.object.ObjectManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import com.netflix.config.DynamicStringListProperty;
 
 public class AuthDaoImpl extends AbstractJooqDao implements AuthDao {
 
     private DynamicStringListProperty SUPPORTED_TYPES = ArchaiusUtil.getList("account.by.key.credential.types");
 
+    GenericResourceDao resourceDao;
+    ObjectManager objectManager;
+    
     @Override
     public Account getAdminAccount() {
         return create()
@@ -40,6 +49,55 @@ public class AuthDaoImpl extends AbstractJooqDao implements AuthDao {
                     .and(CREDENTIAL.SECRET_VALUE.eq(secretKey)))
                     .and(CREDENTIAL.KIND.in(SUPPORTED_TYPES.get()))
                 .fetchOneInto(AccountRecord.class);
+    }
+
+    @Override
+    public Account getAccountByExternalId(String externalId, String externalType) {
+        return create()
+                .selectFrom(ACCOUNT)
+                .where(
+                        ACCOUNT.EXTERNAL_ID.eq(externalId)
+                        .and(ACCOUNT.EXTERNAL_ID_TYPE.eq(externalType))
+                        .and(ACCOUNT.STATE.eq("active"))
+                ).orderBy(ACCOUNT.ID.asc()).limit(1).fetchOne();
+    }
+    
+    @Override
+    public Account createAccount(String name, String kind, String externalId, String externalType) {
+        Map<String, Object> properties =  new HashMap<>();
+        properties.put(ACCOUNT.NAME.toString(), name);
+        properties.put(ACCOUNT.KIND.toString(), kind);
+        properties.put(ACCOUNT.EXTERNAL_ID.toString(), externalId);
+        properties.put(ACCOUNT.EXTERNAL_ID_TYPE.toString(), externalType);
+        return resourceDao.createAndSchedule(Account.class, properties);
+    }
+    
+    @Override
+    public Account getAccountByUuid(String uuid) {
+        return create()
+                .selectFrom(ACCOUNT)
+                .where(
+                        ACCOUNT.UUID.eq(uuid)
+                        .and(ACCOUNT.STATE.eq("active"))
+                        ).orderBy(ACCOUNT.ID.asc()).limit(1).fetchOne();
+    }
+    
+    public ObjectManager getObjectManager() {
+        return objectManager;
+    }
+
+    @Inject
+    public void setObjectManager(ObjectManager objectManager) {
+        this.objectManager = objectManager;
+    }
+    
+    public GenericResourceDao getGenericResourceDao() {
+        return resourceDao;
+    }
+    
+    @Inject
+    public void setGenericResourceDao(GenericResourceDao resourceDao) {
+        this.resourceDao = resourceDao;
     }
 
 }
