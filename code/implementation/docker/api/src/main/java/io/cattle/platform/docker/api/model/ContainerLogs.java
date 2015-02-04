7c34aeb5cbe89aabd7e021507f2d1f8d308501c3
James Harris <james@rancher.com>
2015-02-04 16:07:55 -0700

    Change logs

--- code/implementation/docker/api/src/main/java/io/cattle/platform/docker/api/model/ContainerLogs.java
+++ code/implementation/docker/api/src/main/java/io/cattle/platform/docker/api/model/ContainerLogs.java
@@ -1,12 +1,15 @@
 package io.cattle.platform.docker.api.model;
 
 import io.github.ibuildthecloud.gdapi.annotation.Field;
+import io.github.ibuildthecloud.gdapi.annotation.Type;
 
+@Type(list = false, name = "containerLogs")
 public interface ContainerLogs {
 
-    @Field()
+
+    @Field(defaultValue = "true")
     boolean getFollow();
 
-    @Field()
+    @Field(defaultValue = "100", min = 0)
     Integer getLines();
 }
