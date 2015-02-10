35c68bf45227f2fd6c2c7527acd953ad1afd2c14
Alena Prokharchyk <alena@rancher.com>
2015-02-09 16:59:31 -0800

    Set APIs for LoadBalancer
    loadBalancer.setTargets
    loadBalancerConfig.setListeners

---
+++ code/iaas/logic/src/main/java/io/cattle/platform/process/lb/LoadBalancerConfigSetListeners.java
@@ -0,0 +1,88 @@
+package io.cattle.platform.process.lb;
+
+import static io.cattle.platform.core.model.tables.LoadBalancerConfigListenerMapTable.LOAD_BALANCER_CONFIG_LISTENER_MAP;
+import io.cattle.platform.core.constants.LoadBalancerConstants;
+import io.cattle.platform.core.dao.GenericMapDao;
+import io.cattle.platform.core.model.LoadBalancerConfig;
+import io.cattle.platform.core.model.LoadBalancerConfigListenerMap;
+import io.cattle.platform.core.model.LoadBalancerListener;
+import io.cattle.platform.engine.handler.HandlerResult;
+import io.cattle.platform.engine.process.ProcessInstance;
+import io.cattle.platform.engine.process.ProcessState;
+import io.cattle.platform.json.JsonMapper;
+import io.cattle.platform.object.util.DataAccessor;
+import io.cattle.platform.process.common.handler.AbstractObjectProcessHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+public class LoadBalancerConfigSetListeners extends AbstractObjectProcessHandler {
+
+    @Inject
+    JsonMapper jsonMapper;
+
+    @Inject
+    GenericMapDao mapDao;
+
+    @Override
+    public String[] getProcessNames() {
+        return new String[] { LoadBalancerConstants.PROCESS_LB_CONFIG_SET_LISTENERS };
+    }
+
+    @Override
+    public HandlerResult handle(ProcessState state, ProcessInstance process) {
+        LoadBalancerConfig config = (LoadBalancerConfig) state.getResource();
+        List<? extends Long> newListenerIds = DataAccessor.fromMap(state.getData())
+                .withKey(LoadBalancerConstants.FIELD_LB_LISTENER_IDS).asList(jsonMapper, Long.class);
+
+        if (newListenerIds != null) {
+            // remove old listeners set
+            removeOldListenerMaps(config, newListenerIds);
+
+            // create a new set
+            createNewListenerMaps(config, newListenerIds);
+        }
+        return null;
+    }
+
+    private void createNewListenerMaps(LoadBalancerConfig config, List<? extends Long> newListenerIds) {
+        for (Long listenerId : newListenerIds) {
+            LoadBalancerConfigListenerMap configListenerMap = mapDao.findNonRemoved(
+                    LoadBalancerConfigListenerMap.class,
+                    LoadBalancerConfig.class, config.getId(), LoadBalancerListener.class, listenerId);
+            if (configListenerMap == null) {
+                configListenerMap = objectManager.create(LoadBalancerConfigListenerMap.class,
+                        LOAD_BALANCER_CONFIG_LISTENER_MAP.LOAD_BALANCER_CONFIG_ID, config.getId(),
+                        LOAD_BALANCER_CONFIG_LISTENER_MAP.LOAD_BALANCER_LISTENER_ID, listenerId);
+            }
+            objectProcessManager.executeProcess(
+                    LoadBalancerConstants.PROCESS_LB_CONFIG_LISTENER_MAP_CREATE,
+                    configListenerMap, null);
+        }
+    }
+
+    private void removeOldListenerMaps(LoadBalancerConfig config, List<? extends Long> newListenerIds) {
+        List<? extends LoadBalancerConfigListenerMap> existingMaps = mapDao.findToRemove(
+                LoadBalancerConfigListenerMap.class,
+                LoadBalancerConfig.class, config.getId());
+
+        List<LoadBalancerConfigListenerMap> mapsToRemove = new ArrayList<>();
+
+        for (LoadBalancerConfigListenerMap existingListenerId : existingMaps) {
+            if (!newListenerIds.contains(existingListenerId.getLoadBalancerListenerId())) {
+                mapsToRemove.add(existingListenerId);
+            }
+        }
+
+        for (LoadBalancerConfigListenerMap mapToRemove : mapsToRemove) {
+            objectProcessManager.executeProcess(
+                    LoadBalancerConstants.PROCESS_LB_CONFIG_LISTENER_MAP_REMOVE,
+                    mapToRemove, null);
+        }
+    }
+
+}
--- tests/integration/cattletest/core/test_lb_config.py
+++ tests/integration/cattletest/core/test_lb_config.py
@@ -9,27 +9,8 @@ def test_lb_config_create_wo_listener(admin_client):
     assert config.state == 'active'
 
 
-def create_valid_listener(admin_client):
-    listener = admin_client.create_loadBalancerListener(name=random_str(),
-                                                        sourcePort='8080',
-                                                        targetPort='80',
-                                                        sourceProtocol='http',
-                                                        targetProtocol='tcp')
-    listener = admin_client.wait_success(listener)
-    return listener
-
-
-def create_config_and_listener(admin_client):
-    # create listener
-    listener = create_valid_listener(admin_client)
-    # create config
-    config = admin_client.create_loadBalancerConfig(name=random_str())
-    config = admin_client.wait_success(config)
-    return config, listener
-
-
 def test_lb_config_add_listener(admin_client, super_client):
-    config, listener = create_config_and_listener(admin_client)
+    config, listener = _create_config_and_listener(admin_client)
 
     # add listener to the config
     config = config.addlistener(loadBalancerListenerId=listener.id)
@@ -41,10 +22,11 @@ def test_lb_config_add_listener(admin_client, super_client):
                                            loadBalancerConfigId=config.id)
 
     assert len(lbconfigmap) == 1
+    assert lbconfigmap[0].state == 'active'
 
 
 def test_create_config_add_invalid_listener(admin_client):
-    config, listener = create_config_and_listener(admin_client)
+    config, listener = _create_config_and_listener(admin_client)
 
     with pytest.raises(ApiError) as e:
         config = config.addlistener(loadBalancerListenerId='dummy')
@@ -55,7 +37,7 @@ def test_create_config_add_invalid_listener(admin_client):
 
 
 def test_lb_config_add_listener_wo_listenerId(admin_client):
-    config, listener = create_config_and_listener(admin_client)
+    config, listener = _create_config_and_listener(admin_client)
 
     # add listener to the config without specifying the id
     with pytest.raises(ApiError) as e:
@@ -67,7 +49,7 @@ def test_lb_config_add_listener_wo_listenerId(admin_client):
 
 
 def test_lb_config_remove_listener(admin_client, super_client):
-    config, listener = create_config_and_listener(admin_client)
+    config, listener = _create_config_and_listener(admin_client)
 
     # add listener to the config
     config = config.addlistener(loadBalancerListenerId=listener.id)
@@ -93,7 +75,7 @@ def test_lb_config_remove_listener(admin_client, super_client):
 
 
 def test_lb_config_delete_listener_wo_listenerId(admin_client):
-    config, listener = create_config_and_listener(admin_client)
+    config, listener = _create_config_and_listener(admin_client)
 
     # remove listener to the config without specifying the id
     with pytest.raises(ApiError) as e:
@@ -123,7 +105,7 @@ def test_lb_config_remove(admin_client):
 
 def test_lb_config_listener_remove(admin_client, super_client):
     # create config and listener
-    config, listener = create_config_and_listener(admin_client)
+    config, listener = _create_config_and_listener(admin_client)
 
     # add a listener to the config
     config = config.addlistener(loadBalancerListenerId=listener.id)
@@ -143,7 +125,7 @@ def test_lb_config_listener_remove(admin_client, super_client):
 
 def test_lb_listener_remove(admin_client, super_client):
     # create a listener
-    listener = create_valid_listener(admin_client)
+    listener = _create_valid_listener(admin_client)
 
     # create 2 configs, add listener to both of them
     config1 = admin_client. \
@@ -180,7 +162,7 @@ def test_lb_listener_remove(admin_client, super_client):
 
 def test_lb_config_remove_w_listeners(admin_client, super_client):
     # create a listener
-    listener = create_valid_listener(admin_client)
+    listener = _create_valid_listener(admin_client)
 
     # create 2 configs, add listener to both of them
     config1 = admin_client. \
@@ -243,3 +225,69 @@ def test_lb_config_remove_when_used_by_removed_lb(admin_client):
     config = admin_client.wait_success(admin_client.delete(config))
 
     assert config.state == "removed"
+
+
+def test_lb_config_set_listeners(admin_client, super_client):
+    config, listener1 = _create_config_and_listener(admin_client)
+    listener2 = _create_valid_listener(admin_client)
+
+    # set 2 listeners
+    config = config.setlisteners(
+        loadBalancerListenerIds=[listener1.id, listener2.id])
+    config = admin_client.wait_success(config)
+    assert config.state == 'active'
+
+    lb_config_map = super_client. \
+        list_loadBalancerConfigListenerMap(loadBalancerListenerId=listener1.id,
+                                           loadBalancerConfigId=config.id)
+    assert len(lb_config_map) == 1
+    assert lb_config_map[0].state == 'active'
+
+    lb_config_map = super_client. \
+        list_loadBalancerConfigListenerMap(loadBalancerListenerId=listener2.id,
+                                           loadBalancerConfigId=config.id)
+    assert len(lb_config_map) == 1
+    assert lb_config_map[0].state == 'active'
+
+    # # set 1 listener
+    config = config.setlisteners(loadBalancerListenerIds=[listener1.id])
+    config = admin_client.wait_success(config)
+    lb_config_map = super_client. \
+        list_loadBalancerConfigListenerMap(loadBalancerListenerId=listener2.id,
+                                           loadBalancerConfigId=config.id)
+
+    assert lb_config_map[0].state == 'removed'
+
+    lb_config_map = super_client. \
+        list_loadBalancerConfigListenerMap(loadBalancerListenerId=listener1.id,
+                                           loadBalancerConfigId=config.id)
+
+    assert lb_config_map[0].state == 'active'
+
+    # set 0 listener
+    config = config.setlisteners(loadBalancerListenerIds=[])
+    admin_client.wait_success(config)
+    lb_config_map = super_client. \
+        list_loadBalancerConfigListenerMap(loadBalancerListenerId=listener1.id,
+                                           loadBalancerConfigId=config.id)
+
+    assert lb_config_map[0].state == 'removed'
+
+
+def _create_valid_listener(admin_client):
+    listener = admin_client.create_loadBalancerListener(name=random_str(),
+                                                        sourcePort='8080',
+                                                        targetPort='80',
+                                                        sourceProtocol='http',
+                                                        targetProtocol='tcp')
+    listener = admin_client.wait_success(listener)
+    return listener
+
+
+def _create_config_and_listener(admin_client):
+    # create listener
+    listener = _create_valid_listener(admin_client)
+    # create config
+    config = admin_client.create_loadBalancerConfig(name=random_str())
+    config = admin_client.wait_success(config)
+    return config, listener
