0a34b666b82d7be7962f1c9e800d11be4dfc356d
Darren Shepherd <darren@rancher.com>
2015-04-26 22:12:23 -0700

    disable node-agent, dnsmasq and enable dns

--- resources/content/config-content/dnsmasq/apply.sh
+++ resources/content/config-content/dnsmasq/apply.sh
@@ -2,28 +2,13 @@
 
 . ${CATTLE_HOME:-/var/lib/cattle}/common/scripts.sh
 
-mkdir -p /run/resolvconf/interface
-
-stage_files
-
-if /etc/init.d/dnsmasq status; then
-    # This weirdness is because dnsmasq restart sleeps for 2 seconds, at which time
-    # monit usually restarts it.  So the restart fails because it looks like it never
-    # stopped, but it did actually restart
-    /etc/init.d/dnsmasq restart || /etc/init.d/dnsmasq status
-else
-    /etc/init.d/dnsmasq start
+if [ -e /etc/init.d/dnsmasq ]; then
+    /etc/init.d/dnsmasq stop || true
+    killall -9 dnsmasq || true
 fi
 
-for i in {1..100}; do
-    if [ "$(pidof dnsmasq)" = "" ]; then
-        sleep .1
-    else
-        break
-    fi
-done
-
-if [ "$(pidof dnsmasq)" = "" ]; then
-    error "Failed to start dnsmasq"
-    exit 1
+if [ -e /etc/monit/conf.d/dnsmasq ]; then
+    monit unmonitor dnsmasq || true
+    rm /etc/monit/conf.d/dnsmasq
+    monit reload
 fi
