c2d2a0fcb5e9d906f99f3b1c001fa77bf2d906cc
Alena Prokharchyk <alena@rancher.com>
2015-04-28 10:12:22 -0700

    Added "-" as a valid char for env/service name
    Names can have "-" in them; but can't start or end with -

--- code/iaas/service-discovery/api/src/main/java/io/cattle/platform/servicediscovery/api/filter/EnvironmentCreateValidationFilter.java
+++ code/iaas/service-discovery/api/src/main/java/io/cattle/platform/servicediscovery/api/filter/EnvironmentCreateValidationFilter.java
@@ -1,7 +1,7 @@
 package io.cattle.platform.servicediscovery.api.filter;
 
-import static io.cattle.platform.core.model.tables.ServiceTable.SERVICE;
-import io.cattle.platform.core.model.Service;
+import static io.cattle.platform.core.model.tables.EnvironmentTable.ENVIRONMENT;
+import io.cattle.platform.core.model.Environment;
 import io.cattle.platform.iaas.api.filter.common.AbstractDefaultResourceManagerFilter;
 import io.cattle.platform.object.ObjectManager;
 import io.github.ibuildthecloud.gdapi.request.ApiRequest;
@@ -10,28 +10,20 @@ import io.github.ibuildthecloud.gdapi.validation.ValidationErrorCodes;
 
 import javax.inject.Inject;
 
-public class ServiceCreateValidationFilter extends AbstractDefaultResourceManagerFilter {
-
+public class EnvironmentCreateValidationFilter extends AbstractDefaultResourceManagerFilter {
     @Inject
     ObjectManager objectManager;
 
     @Override
     public Class<?>[] getTypeClasses() {
-        return new Class<?>[] { Service.class };
+        return new Class<?>[] { Environment.class };
     }
 
     @Override
     public Object create(String type, ApiRequest request, ResourceManager next) {
-        Service service = request.proxyRequestObject(Service.class);
-        
-        Service existingService = objectManager.findOne(Service.class, SERVICE.NAME, service.getName(),
-                SERVICE.ENVIRONMENT_ID, service.getEnvironmentId(), SERVICE.REMOVED, null);
-        if (existingService != null) {
-            ValidationErrorCodes.throwValidationError(ValidationErrorCodes.NOT_UNIQUE,
-                    "name");
-        }
+        Environment env = request.proxyRequestObject(Environment.class);
 
-        if (service.getName().startsWith("-") || service.getName().endsWith("-")) {
+        if (env.getName().startsWith("-") || env.getName().endsWith("-")) {
             ValidationErrorCodes.throwValidationError(ValidationErrorCodes.INVALID_CHARACTERS,
                     "name");
         }
