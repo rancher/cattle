c3aed2567bd5da7b60f5fc4cd011270b3a56f33f
Craig Jellick <craig@rancher.com>
2015-05-19 07:46:58 -0700

    more debug

--- tests/integration/cattletest/core/test_resource.py
+++
@@ -1,7 +0,0 @@
-from common_fixtures import *  # NOQA
-
-
-# test that admin_client can list accounts
-# (parent of project which has just been created)
-def test_list_account(admin_client):
-    admin_client.list_account()
--- tests/integration/cattletest/core/test_zone.py
+++
@@ -1,6 +0,0 @@
-from common_fixtures import *  # NOQA
-
-
-def test_zone_list(super_client):
-    zones = super_client.list_zone()
-    assert len(zones) > 0
