a30c0fa4cc900ae6441ac62c2fdfbe490eda40df
Darren Shepherd <darren@rancher.com>
2015-06-07 01:15:06 -0700

    Don't assume secretValue is set if publicValue is

--- tests/integration/cattletest/core/test_apikey.py
+++ tests/integration/cattletest/core/test_apikey.py
@@ -27,3 +27,17 @@ def test_api_key_create_admin(client):
     assert key.state == 'active'
     assert key.publicValue is not None
     assert key.secretValue is not None
+
+
+def test_api_key_null_secret(super_client, context):
+    key = super_client.create_api_key(accountId=context.project.id,
+                                      publicValue='foo',
+                                      secretValue=None)
+    assert key.state == 'registering'
+    assert key.publicValue == 'foo'
+    assert key.secretValue is None
+
+    key = super_client.wait_transitioning(key)
+    assert key.state == 'active'
+    assert key.publicValue == 'foo'
+    assert key.secretValue is None
