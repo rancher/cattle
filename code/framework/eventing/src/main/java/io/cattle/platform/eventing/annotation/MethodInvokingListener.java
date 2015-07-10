fe472216b3397287dc21e3753ef355a5654c9d2a
Alena Prokharchyk <alena@rancher.com>
2015-07-10 15:56:36 -0700

    Log TimeoutException in INFO

--- code/framework/eventing/src/main/java/io/cattle/platform/eventing/annotation/MethodInvokingListener.java
+++ code/framework/eventing/src/main/java/io/cattle/platform/eventing/annotation/MethodInvokingListener.java
@@ -1,5 +1,6 @@
 package io.cattle.platform.eventing.annotation;
 
+import io.cattle.platform.async.utils.TimeoutException;
 import io.cattle.platform.eventing.EventListener;
 import io.cattle.platform.eventing.PoolSpecificListener;
 import io.cattle.platform.eventing.model.Event;
@@ -7,13 +8,19 @@ import io.cattle.platform.json.JsonMapper;
 import io.cattle.platform.lock.LockCallbackNoReturn;
 import io.cattle.platform.lock.LockManager;
 import io.cattle.platform.lock.definition.LockDefinition;
+import io.cattle.platform.lock.exception.FailedToAcquireLockException;
 import io.cattle.platform.util.exception.ExceptionUtils;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MethodInvokingListener implements EventListener, PoolSpecificListener {
+
+    private static final Logger log = LoggerFactory.getLogger(MethodInvokingListener.class);
 
     LockManager lockManager;
     JsonMapper jsonMapper;
@@ -86,6 +93,8 @@ public class MethodInvokingListener implements EventListener, PoolSpecificListen
         } catch (InvocationTargetException e) {
             ExceptionUtils.rethrowRuntime(e.getCause());
             throw new IllegalStateException("Failed to invoke method [" + method + "] for event [" + event + "", e);
+        } catch (TimeoutException | FailedToAcquireLockException e) {
+            log.info(e.getMessage() + " for event [" + event + "]");
         }
     }
 
