3db1b14f58b3f754eff7813be23da73eed0c9c91
sidharthamani <sid@rancher.com>
2015-08-06 14:39:44 -0700

    add support for aggregated stats

--- code/implementation/host-stats/src/main/java/io/cattle/platform/host/stats/api/HostStatsLinkHandler.java
+++ code/implementation/host-stats/src/main/java/io/cattle/platform/host/stats/api/HostStatsLinkHandler.java
@@ -1,93 +1,102 @@
 package io.cattle.platform.host.stats.api;
 
 import io.cattle.platform.api.link.LinkHandler;
-import io.cattle.platform.archaius.util.ArchaiusUtil;
+import io.cattle.platform.core.constants.AccountConstants;
+import io.cattle.platform.core.constants.CommonStatesConstants;
 import io.cattle.platform.core.constants.HostConstants;
-import io.cattle.platform.core.constants.InstanceConstants;
+import io.cattle.platform.core.constants.ProjectConstants;
+import io.cattle.platform.core.model.Account;
 import io.cattle.platform.core.model.Host;
-import io.cattle.platform.core.model.Instance;
-import io.cattle.platform.docker.util.DockerUtils;
 import io.cattle.platform.host.model.HostApiAccess;
 import io.cattle.platform.host.service.HostApiService;
-import io.cattle.platform.host.stats.utils.HostStatsConstants;
+import io.cattle.platform.host.stats.utils.StatsConstants;
 import io.cattle.platform.object.ObjectManager;
+import io.cattle.platform.token.TokenService;
+import io.github.ibuildthecloud.gdapi.context.ApiContext;
 import io.github.ibuildthecloud.gdapi.request.ApiRequest;
 
 import java.io.IOException;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
-import com.netflix.config.DynamicStringProperty;
-
 public class HostStatsLinkHandler implements LinkHandler {
 
-    private static final DynamicStringProperty HOST_STATS_PATH = ArchaiusUtil.getString("host.stats.path");
+    private static final String PROJECT_PATH = "/project/";
 
+    @Inject
     HostApiService hostApiService;
+    @Inject
     ObjectManager objectManager;
+    @Inject
+    TokenService tokenService;
 
     @Override
     public String[] getTypes() {
-        return new String[] { InstanceConstants.TYPE_CONTAINER, HostConstants.TYPE };
+        return new String[] { HostConstants.TYPE, ProjectConstants.TYPE };
     }
 
     @Override
     public boolean handles(String type, String id, String link, ApiRequest request) {
-        return HostStatsConstants.LINK_STATS.equals(link);
+        return StatsConstants.HOST_STATS.equalsIgnoreCase(link) && (ProjectConstants.TYPE.equalsIgnoreCase(type) || HostConstants.TYPE.equalsIgnoreCase(type));
     }
 
     @Override
     public Object link(String name, Object obj, ApiRequest request) throws IOException {
-        Host host = null;
-        Instance instance = null;
-
-        if (obj instanceof Instance) {
-            instance = (Instance) obj;
-            host = DockerUtils.getHostFromContainer(objectManager, instance);
-        } else if (obj instanceof Host) {
-            host = (Host) obj;
+        List<Host> hosts = new ArrayList<>();
+        boolean project = false;
+        if (obj instanceof Host) {
+            Host origHost = (Host) obj;
+            hosts.add(origHost);
         }
 
-        if (host == null) {
-            return null;
+        if (obj instanceof Account && AccountConstants.PROJECT_KIND.equals(((Account) obj).getKind())) {
+            hosts.addAll(objectManager.mappedChildren(obj, Host.class));
+            project = true;
         }
 
-        String[] pathSegments = null;
-        if (instance != null) {
-            pathSegments = new String[] { HOST_STATS_PATH.get(), DockerUtils.getDockerIdentifier(instance) };
-        } else {
-            pathSegments = new String[] { HOST_STATS_PATH.get() };
+        if (hosts.size() == 0) {
+            return new StatsAccess();
         }
 
-        HostApiAccess apiAccess = hostApiService.getAccess(request, host.getId(), Collections.<String, Object> emptyMap(), pathSegments);
-        if (apiAccess == null) {
-            return null;
+        String metaUrl = null;
+        StatsAccess meta = new StatsAccess();
+        List<StatsAccess> serviceStatsQuery = new ArrayList<>();
+        for (Host host : hosts) {
+            if (!CommonStatesConstants.ACTIVE.equals(host.getState())) {
+                continue;
+            }
+            Map<String, Object> payload = new HashMap<>();
+            payload.put("resourceId", ApiContext.getContext().getIdFormatter().formatId(objectManager.getType(host), host.getId()));
+            HostApiAccess apiAccess = hostApiService.getAccess(request, host.getId(), payload, new String[] { StatsConstants.HOST_STATS_PATH.get() });
+            if (apiAccess == null) {
+                continue;
+            }
+
+            String url = apiAccess.getUrl();
+
+            metaUrl = url + PROJECT_PATH;
+
+            StatsAccess statsAccess = new StatsAccess();
+            statsAccess.setToken(apiAccess.getAuthenticationToken());
+            statsAccess.setUrl(url.toString());
+
+            if (!project) {
+                return statsAccess;
+            }
+
+            serviceStatsQuery.add(statsAccess);
         }
+        Map<String, Object> metaQueryPayload = new HashMap<>();
+        metaQueryPayload.put(ProjectConstants.TYPE, serviceStatsQuery);
 
-        StatsAccess statsAccess = new StatsAccess();
-        statsAccess.setToken(apiAccess.getAuthenticationToken());
-        statsAccess.setUrl(apiAccess.getUrl());
+        meta.setToken(tokenService.generateToken(metaQueryPayload));
+        meta.setUrl(metaUrl);
 
-        return statsAccess;
-    }
-
-    public HostApiService getHostApiService() {
-        return hostApiService;
-    }
-
-    @Inject
-    public void setHostApiService(HostApiService hostApiService) {
-        this.hostApiService = hostApiService;
-    }
-
-    public ObjectManager getObjectManager() {
-        return objectManager;
-    }
-
-    @Inject
-    public void setObjectManager(ObjectManager objectManager) {
-        this.objectManager = objectManager;
+        return meta;
     }
 
 }
--- code/implementation/host-stats/src/main/java/io/cattle/platform/host/stats/utils/HostStatsConstants.java
+++
@@ -1,7 +0,0 @@
-package io.cattle.platform.host.stats.utils;
-
-public class HostStatsConstants {
-
-    public static final String LINK_STATS = "stats";
-
-}
