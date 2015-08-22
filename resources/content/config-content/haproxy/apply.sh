3fe2b83ea341a141fe9d4f7894d4a3fc8efc0a47
Darren Shepherd <darren@rancher.com>
2015-08-21 20:54:47 -0700

    Dynamically upgrade to HAProxy 1.5

--- resources/content/config-content/haproxy/apply.sh
+++ resources/content/config-content/haproxy/apply.sh
@@ -8,4 +8,9 @@ awk '/BEGIN RSA PRIVATE KEY/{i++}{print > "/etc/haproxy/certs/server"i".pem"}' /
 rm /etc/haproxy/certs/certs.pem
 if [ ! -s /etc/haproxy/certs/default.pem ]; then rm /etc/haproxy/certs/default.pem;fi
 
+if ! haproxy -v | grep -q 'version 1.5'; then
+    echo deb http://archive.ubuntu.com/ubuntu trusty-backports main universe > /etc/apt/sources.list.d/backports.list
+    apt-get update && apt-get install -y haproxy -t trusty-backports
+fi
+
 reload_haproxy /etc/haproxy/haproxy.cfg
