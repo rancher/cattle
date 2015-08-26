da1e8e5663ac0625fe4a9ba796b53a4454817cf0
Craig Jellick <craig@rancher.com>
2015-08-25 17:07:02 -0700

    Use absolute path for static redirect

--- code/iaas/api-logic/src/main/java/io/cattle/platform/iaas/api/request/handler/UIRedirect.java
+++ code/iaas/api-logic/src/main/java/io/cattle/platform/iaas/api/request/handler/UIRedirect.java
@@ -6,23 +6,22 @@ import io.github.ibuildthecloud.gdapi.request.handler.AbstractResponseGenerator;
 import io.github.ibuildthecloud.gdapi.request.parser.DefaultApiRequestParser;
 
 import java.io.IOException;
+import java.net.URL;
 
 import com.netflix.config.DynamicBooleanProperty;
-import com.netflix.config.DynamicStringProperty;
 
 public class UIRedirect extends AbstractResponseGenerator {
 
     private static final DynamicBooleanProperty DO_REDIRECT = ArchaiusUtil.getBoolean("api.redirect.to.ui");
-    private static final DynamicStringProperty UI_REDIRECT = ArchaiusUtil.getString("api.redirect.ui.path");
 
     @Override
     protected void generate(ApiRequest request) throws IOException {
         if (request.getRequestVersion() != null || !DO_REDIRECT.get() || !DefaultApiRequestParser.HTML.equals(request.getResponseFormat()))
             return;
 
-        request.getServletContext().getResponse().sendRedirect(UI_REDIRECT.get());
+        URL url = request.getUrlBuilder().staticResource();
+        request.getServletContext().getResponse().sendRedirect(url.toExternalForm());
         request.setResponseObject(new Object());
         request.commit();
     }
-
 }
