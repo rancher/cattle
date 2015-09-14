4cf89c14b5766aa3caeace5673325ffa960e97f8
Craig Jellick <craig@rancher.com>
2015-09-14 14:56:41 -0700

    Add logging

--- tests/integration/cattletest/core/test_subscribe.py
+++ tests/integration/cattletest/core/test_subscribe.py
@@ -2,6 +2,7 @@ from common_fixtures import *  # NOQA
 
 import websocket
 import time
+import logging
 
 SUB_OPTIONS = '?include=hosts&include=instances&include=instanceLinks' \
               '&include=ipAddresses&eventNames=resource.change&projectId=%s'
@@ -11,16 +12,20 @@ def test_websocket_close(client, context):
     assertions = {}
 
     def on_message(ws, message):
+        logging.warn("CAJ got message: " + message)
         assertions['messaged'] = True
         ws.close()
 
     def on_error(ws, error):
+        logging.warn("CAJ got error")
         assert False, "Got an error: %s" % error
 
     def on_close(ws):
+        logging.warn("CAJ got close")
         assertions['closed'] = True
 
     def on_open(ws):
+        logging.warn("CAJ got open")
         assertions['opened'] = True
 
     websocket.enableTrace(True)
