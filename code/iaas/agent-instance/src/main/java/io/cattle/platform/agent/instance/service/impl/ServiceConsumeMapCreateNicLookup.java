798884b3de23d116e28f47573098ac69cf642cd5
Alena Prokharchyk <alena@rancher.com>
2015-11-12 11:16:10 -0800

    On any service/instance related operation, trigger updates
    for all vnets in the system. As all changes should be propagated for
    dns/metadata to all the hosts

--- code/iaas/agent-instance/src/main/java/io/cattle/platform/agent/instance/service/impl/ServiceConsumeMapCreateNicLookup.java
+++ code/iaas/agent-instance/src/main/java/io/cattle/platform/agent/instance/service/impl/ServiceConsumeMapCreateNicLookup.java
@@ -1,23 +1,16 @@
 package io.cattle.platform.agent.instance.service.impl;
 
-import static io.cattle.platform.core.model.tables.InstanceTable.INSTANCE;
-import static io.cattle.platform.core.model.tables.NicTable.NIC;
-import static io.cattle.platform.core.model.tables.ServiceExposeMapTable.SERVICE_EXPOSE_MAP;
-import static io.cattle.platform.core.model.tables.ServiceConsumeMapTable.*;
 import io.cattle.platform.agent.instance.service.InstanceNicLookup;
 import io.cattle.platform.core.dao.GenericMapDao;
 import io.cattle.platform.core.model.Nic;
-import io.cattle.platform.core.model.Service;
 import io.cattle.platform.core.model.ServiceConsumeMap;
-import io.cattle.platform.core.model.tables.records.NicRecord;
-import io.cattle.platform.db.jooq.dao.impl.AbstractJooqDao;
 import io.cattle.platform.object.ObjectManager;
 
 import java.util.List;
 
 import javax.inject.Inject;
 
-public class ServiceConsumeMapCreateNicLookup extends AbstractJooqDao implements InstanceNicLookup {
+public class ServiceConsumeMapCreateNicLookup extends NicPerVnetNicLookup implements InstanceNicLookup {
     @Inject
     ObjectManager objectManager;
 
@@ -31,40 +24,6 @@ public class ServiceConsumeMapCreateNicLookup extends AbstractJooqDao implements
         }
 
         ServiceConsumeMap consumeMap = (ServiceConsumeMap) obj;
-        Service service = objectManager.loadResource(Service.class, consumeMap.getServiceId());
-        
-        // hardcoding the value to mvn projects cross reference
-        if (service.getKind().equalsIgnoreCase("dnsService")) {
-            // get the nics of the instances of the services consuming the DNS service
-            return create().
-                    select(NIC.fields()).
-                    from(NIC)
-                    .join(INSTANCE)
-                    .on(INSTANCE.ID.eq(NIC.INSTANCE_ID))
-                    .join(SERVICE_EXPOSE_MAP)
-                    .on(SERVICE_EXPOSE_MAP.INSTANCE_ID.eq(INSTANCE.ID))
-                    .join(SERVICE_CONSUME_MAP)
-                    .on(SERVICE_CONSUME_MAP.SERVICE_ID.eq(SERVICE_EXPOSE_MAP.SERVICE_ID))
-                    .where(SERVICE_CONSUME_MAP.CONSUMED_SERVICE_ID.eq(service.getId())
-                            .and(NIC.REMOVED.isNull())
-                            .and(INSTANCE.REMOVED.isNull())
-                            .and(SERVICE_EXPOSE_MAP.REMOVED.isNull())
-                            .and(SERVICE_CONSUME_MAP.REMOVED.isNull()))
-                    .fetchInto(NicRecord.class);
-        } else {
-            // get nics of instances that are the part of the service
-            return create().
-                    select(NIC.fields()).
-                    from(NIC)
-                    .join(INSTANCE)
-                    .on(INSTANCE.ID.eq(NIC.INSTANCE_ID))
-                    .join(SERVICE_EXPOSE_MAP)
-                    .on(SERVICE_EXPOSE_MAP.INSTANCE_ID.eq(INSTANCE.ID))
-                    .where(SERVICE_EXPOSE_MAP.SERVICE_ID.eq(service.getId())
-                            .and(NIC.REMOVED.isNull())
-                            .and(INSTANCE.REMOVED.isNull())
-                            .and(SERVICE_EXPOSE_MAP.REMOVED.isNull()))
-                    .fetchInto(NicRecord.class);
-        }
+        return super.getNicPerVnetForAccount(consumeMap.getAccountId());
     }
 }
