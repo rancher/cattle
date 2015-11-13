eab2b89b94fcebc01306e91a8817ef990e1fb9cf
Alena Prokharchyk <alena@rancher.com>
2015-11-13 11:08:08 -0800

    Set startCount to 1 on the first instance start

--- code/iaas/healthcheck/src/main/java/io/cattle/iaas/healthcheck/process/InstanceHealthcheckRegister.java
+++ code/iaas/healthcheck/src/main/java/io/cattle/iaas/healthcheck/process/InstanceHealthcheckRegister.java
@@ -45,7 +45,7 @@ public class InstanceHealthcheckRegister extends AbstractObjectProcessLogic impl
                 InstanceConstants.FIELD_HEALTH_CHECK, jsonMapper, InstanceHealthCheck.class);
 
         // set healthcheck
-        Long startCount = instance.getStartCount() == null ? 0 : instance.getStartCount() + 1;
+        Long startCount = instance.getStartCount() == null ? 1 : instance.getStartCount() + 1;
         if (healthCheck != null) {
             objectManager.setFields(instance, INSTANCE.START_COUNT, startCount, INSTANCE.HEALTH_STATE,
                     HealthcheckConstants.HEALTH_STATE_INITIALIZING);
