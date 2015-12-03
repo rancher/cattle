ef5fe636daef23aec6010fb95e79a53cf560cec6
Alena Prokharchyk <alena@rancher.com>
2015-12-02 19:09:25 -0800

    Update metadata on instancehostmap.activate

--- code/iaas/agent-instance/src/main/java/io/cattle/platform/agent/instance/service/impl/InstanceHostMapNicLookup.java
+++ code/iaas/agent-instance/src/main/java/io/cattle/platform/agent/instance/service/impl/InstanceHostMapNicLookup.java
@@ -1,15 +1,19 @@
 package io.cattle.platform.agent.instance.service.impl;
 
-import static io.cattle.platform.core.model.tables.NicTable.*;
 import io.cattle.platform.agent.instance.service.InstanceNicLookup;
+import io.cattle.platform.core.model.Instance;
 import io.cattle.platform.core.model.InstanceHostMap;
 import io.cattle.platform.core.model.Nic;
-import io.cattle.platform.db.jooq.dao.impl.AbstractJooqDao;
+import io.cattle.platform.object.ObjectManager;
 
 import java.util.Collections;
 import java.util.List;
 
-public class InstanceHostMapNicLookup extends AbstractJooqDao implements InstanceNicLookup {
+import javax.inject.Inject;
+
+public class InstanceHostMapNicLookup extends NicPerVnetNicLookup implements InstanceNicLookup {
+    @Inject
+    ObjectManager objectManager;
 
     @Override
     public List<? extends Nic> getNics(Object obj) {
@@ -22,8 +26,11 @@ public class InstanceHostMapNicLookup extends AbstractJooqDao implements Instanc
         if (map.getInstanceId() == null) {
             return Collections.emptyList();
         }
-
-        return create().selectFrom(NIC).where(NIC.INSTANCE_ID.eq(map.getInstanceId())).fetch();
+        Instance instance = objectManager.loadResource(Instance.class, map.getInstanceId());
+        if (instance != null) {
+            return super.getNicPerVnetForAccount(instance.getAccountId());
+        }
+        return null;
     }
 
 }
