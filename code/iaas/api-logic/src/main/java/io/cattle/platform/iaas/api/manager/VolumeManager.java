2154c820ffe544dc66fa3275fbe0b3be88d6987e
Craig Jellick <craig@rancher.com>
2016-01-15 12:26:57 -0700

    Use standard resource delete behavior
    Bonus: Schedule (instead of execut) mount.create

--- code/iaas/api-logic/src/main/java/io/cattle/platform/iaas/api/manager/VolumeManager.java
+++ code/iaas/api-logic/src/main/java/io/cattle/platform/iaas/api/manager/VolumeManager.java
@@ -1,13 +1,9 @@
 package io.cattle.platform.iaas.api.manager;
 
 import io.cattle.platform.api.resource.jooq.AbstractJooqResourceManager;
-import io.cattle.platform.core.constants.CommonStatesConstants;
 import io.cattle.platform.core.constants.VolumeConstants;
 import io.cattle.platform.core.model.Volume;
 import io.cattle.platform.object.meta.ObjectMetaDataManager;
-import io.cattle.platform.object.process.StandardProcess;
-import io.cattle.platform.util.type.CollectionUtils;
-import io.github.ibuildthecloud.gdapi.request.ApiRequest;
 
 import java.util.Map;
 
@@ -36,21 +32,4 @@ public class VolumeManager extends AbstractJooqResourceManager {
         }
         return getObjectManager().create(clz, properties);
     }
-
-    @Override
-    protected Object deleteInternal(String type, String id, Object obj, ApiRequest request) {
-        if (!(obj instanceof Volume)) {
-            return super.deleteInternal(type, id, obj, request);
-        }
-
-        Volume volume = (Volume) obj;
-
-        if (CommonStatesConstants.ACTIVE.equals(volume.getState())) {
-            scheduleProcess(StandardProcess.DEACTIVATE, obj, CollectionUtils.asMap(VolumeConstants.REMOVE_OPTION, true));
-            return getObjectManager().reload(obj);
-        } else {
-            return super.deleteInternal(type, id, obj, request);
-        }
-    }
-
 }
