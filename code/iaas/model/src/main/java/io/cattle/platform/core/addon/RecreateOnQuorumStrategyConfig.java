85a1142b54cb0d95ca1b42f38ece4d934f3cfb8d
Alena Prokharchyk <alena@rancher.com>
2016-01-19 11:26:53 -0800

    Default healthcheck quorum to 1 if not specified

--- code/iaas/model/src/main/java/io/cattle/platform/core/addon/RecreateOnQuorumStrategyConfig.java
+++ code/iaas/model/src/main/java/io/cattle/platform/core/addon/RecreateOnQuorumStrategyConfig.java
@@ -5,6 +5,8 @@ import io.github.ibuildthecloud.gdapi.annotation.Type;
 
 @Type(list = false)
 public class RecreateOnQuorumStrategyConfig {
+    private static Integer defaultQuorum = 1;
+
     Integer quorum;
 
     public RecreateOnQuorumStrategyConfig() {
@@ -16,7 +18,7 @@ public class RecreateOnQuorumStrategyConfig {
 
     @Field(nullable = false, required = true)
     public Integer getQuorum() {
-        return quorum;
+        return quorum != null ? quorum : defaultQuorum;
     }
 
     public void setQuorum(Integer quorum) {
