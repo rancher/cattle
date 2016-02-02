05d98e2810ce6a76b2d80adeddafe8964a083834
James Jeffrey <wizardofmath@gmail.com>
2016-02-02 14:19:53 -0700

    Only remap on event.

--- code/framework/auditing/src/main/java/io/cattle/platform/iaas/api/auditing/AuditServiceImpl.java
+++ code/framework/auditing/src/main/java/io/cattle/platform/iaas/api/auditing/AuditServiceImpl.java
@@ -96,8 +96,8 @@ public class AuditServiceImpl implements AuditService{
         long runtime = ((long) request.getAttribute("requestEndTime")) - ((long)request.getAttribute("requestStartTime"));
         String authType = (String) request.getAttribute(AccountConstants.AUTH_TYPE);
         String resourceId =request.getResponseObject() instanceof Resource ? ((Resource) request.getResponseObject()).getId(): null;
-        String resourceType = convertResourceType(request.getType());
-        String eventType = "api." + resourceType + "." + (StringUtils.isNotBlank(request.getAction()) ?
+        String resourceType = request.getType();
+        String eventType = "api." + convertResourceType(resourceType) + "." + (StringUtils.isNotBlank(request.getAction()) ?
                 request.getAction() :convertToAction(request.getMethod()));
         publishEvent(auditLogDao.create(resourceType, parseId(resourceId), data, user,
                 policy.getAccountId(), policy.getAuthenticatedAsAccountId(), eventType, authType, runtime, null, request.getClientIp()));
