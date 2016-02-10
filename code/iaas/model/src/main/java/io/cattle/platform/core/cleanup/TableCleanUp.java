9715ce0ebd9d6654d8ca8c407a1b53be8e110b8a
James Jeffrey <wizardofmath@gmail.com>
2016-02-10 13:41:40 -0700

    Cleanup item config status table.

--- code/iaas/model/src/main/java/io/cattle/platform/core/cleanup/TableCleanUp.java
+++ code/iaas/model/src/main/java/io/cattle/platform/core/cleanup/TableCleanUp.java
@@ -1,18 +1,26 @@
 package io.cattle.platform.core.cleanup;
 
+import static io.cattle.platform.core.model.tables.AgentTable.AGENT;
 import static io.cattle.platform.core.model.tables.ContainerEventTable.CONTAINER_EVENT;
+import static io.cattle.platform.core.model.tables.ConfigItemStatusTable.CONFIG_ITEM_STATUS;
 import static io.cattle.platform.core.model.tables.ProcessInstanceTable.PROCESS_INSTANCE;
 import static io.cattle.platform.core.model.tables.ServiceEventTable.SERVICE_EVENT;
+import static io.cattle.platform.core.model.tables.ServiceTable.SERVICE;
 import static io.cattle.platform.core.model.tables.AuditLogTable.AUDIT_LOG;
 
 import io.cattle.platform.archaius.util.ArchaiusUtil;
 import io.cattle.platform.core.constants.CommonStatesConstants;
 import io.cattle.platform.db.jooq.dao.impl.AbstractJooqDao;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.jooq.Record1;
+import org.jooq.Result;
+import org.jooq.ResultQuery;
 
 import com.netflix.config.DynamicLongProperty;
 
@@ -61,15 +69,43 @@ public class TableCleanUp extends AbstractJooqDao {
                 .execute();
         long endAuditLog = System.currentTimeMillis();
 
-        if ((deletedContainerEventRecords + deletedProcessInstanceRecords + deletedServiceEventRecords) > 0) {
+        long startConfigItemStatusStart = System.currentTimeMillis();
+
+        int deletedConfigItemStatuses = deleteConfigItemStatus(create().select(CONFIG_ITEM_STATUS.ID).from(CONFIG_ITEM_STATUS)
+                .join(AGENT).on(CONFIG_ITEM_STATUS.AGENT_ID.eq(AGENT.ID)).where(AGENT.REMOVED.isNotNull()).limit(100));
+
+
+        deletedConfigItemStatuses = deletedConfigItemStatuses + deleteConfigItemStatus(create().select(CONFIG_ITEM_STATUS.ID).from(CONFIG_ITEM_STATUS)
+                .join(SERVICE).on(CONFIG_ITEM_STATUS.SERVICE_ID.eq(SERVICE.ID)).where(SERVICE.REMOVED.isNotNull()).limit(100));
+
+        long endConfigItemStatusStart = System.currentTimeMillis();
+
+        if ((deletedContainerEventRecords + deletedProcessInstanceRecords + deletedServiceEventRecords + deletedConfigItemStatuses) > 0) {
             logger.info("Deleted " + deletedProcessInstanceRecords + " from PROCESS_INSTANCE in " +
                     (endProcessInstance - startProcessInstance) + "ms, " +
                     deletedServiceEventRecords + " from SERVICE_EVENT in " + (endServiceEvent - startServiceEvent) +
                     "ms, and " +
-                    deletedContainerEventRecords + " from CONTAINER_EVENT in " + (endContainerEvent - startContainerEvent) + "ms." +
-                    deletedAuditLogRecords + " from CONTAINER_EVENT in " + (endAuditLog - startAuditLog) + "ms.");
+                    deletedContainerEventRecords + " from CONTAINER_EVENT in " + (endContainerEvent - startContainerEvent) + "ms, " +
+                    deletedConfigItemStatuses + " from CONFIG_ITEM_STATUS in " + (endConfigItemStatusStart - startConfigItemStatusStart) + "ms, " +
+                    deletedAuditLogRecords + " from CONTAINER_EVENT in " + (endAuditLog - startAuditLog) + "ms. ");
         }
-        logger.debug("Clean up for PROCESS_INSTANCE, SERVICE_EVENT, CONTAINER_EVENT and AUDIT_LOG tables finished.");
+        logger.debug("Clean up for PROCESS_INSTANCE, SERVICE_EVENT, CONTAINER_EVENT, AUDIT_LOG, and CONFIG_ITEM_STATUS tables finished.");
 
+    }
+
+    private int deleteConfigItemStatus(ResultQuery<Record1<Long>>  query) {
+        Result<Record1<Long>> toDelete;
+        int deletedConfigItemStatuses = 0;
+        while ( (toDelete = query.fetch()).size() > 0 ) {
+            List<Long> idsToDelete = new ArrayList<>();
+            for (Record1<Long> record : toDelete) {
+                idsToDelete.add(record.value1());
+            }
+            deletedConfigItemStatuses += create()
+                    .delete(CONFIG_ITEM_STATUS)
+                    .where(CONFIG_ITEM_STATUS.ID.in(idsToDelete))
+                    .execute();
+        }
+        return deletedConfigItemStatuses;
     }
 }
