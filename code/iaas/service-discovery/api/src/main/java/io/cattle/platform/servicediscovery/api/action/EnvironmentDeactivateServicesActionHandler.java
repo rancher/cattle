6a21915ded2bd4beae67296d6d318b075389850b
Alena Prokharchyk <alena@rancher.com>
2016-02-10 10:45:14 -0800

    Service state validation on environment.deactivateServices
    Also fixed TF in health check happening to instancehostMap.instance
    being resolved to Not found. Underlying problems - why instance was null
    - wasn't fixed; test would just skip problematic host map

--- code/iaas/service-discovery/api/src/main/java/io/cattle/platform/servicediscovery/api/action/EnvironmentDeactivateServicesActionHandler.java
+++ code/iaas/service-discovery/api/src/main/java/io/cattle/platform/servicediscovery/api/action/EnvironmentDeactivateServicesActionHandler.java
@@ -1,13 +1,17 @@
 package io.cattle.platform.servicediscovery.api.action;
 
 import io.cattle.platform.api.action.ActionHandler;
+import io.cattle.platform.core.constants.CommonStatesConstants;
 import io.cattle.platform.core.model.Environment;
 import io.cattle.platform.core.model.Service;
 import io.cattle.platform.object.ObjectManager;
 import io.cattle.platform.object.process.ObjectProcessManager;
 import io.cattle.platform.servicediscovery.api.constants.ServiceDiscoveryConstants;
+import io.github.ibuildthecloud.gdapi.exception.ValidationErrorException;
 import io.github.ibuildthecloud.gdapi.request.ApiRequest;
+import io.github.ibuildthecloud.gdapi.validation.ValidationErrorCodes;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -40,9 +44,20 @@ public class EnvironmentDeactivateServicesActionHandler implements ActionHandler
     }
 
     private void deactivateServices(List<? extends Service> services) {
+        List<String> validStates = Arrays.asList(CommonStatesConstants.ACTIVE, CommonStatesConstants.ACTIVATING,
+                CommonStatesConstants.UPDATING_ACTIVE, CommonStatesConstants.UPDATING_INACTIVE,
+                ServiceDiscoveryConstants.STATE_RESTARTING, CommonStatesConstants.DEACTIVATING);
         for (Service service : services) {
-            objectProcessManager.scheduleProcessInstance(ServiceDiscoveryConstants.PROCESS_SERVICE_DEACTIVATE,
+            if (validStates.contains(service.getState())) {
+                if (!service.getState().equals(CommonStatesConstants.DEACTIVATING)) {
+                    objectProcessManager.scheduleProcessInstance(ServiceDiscoveryConstants.PROCESS_SERVICE_DEACTIVATE,
                             service, null);
+                }
+            } else {
+                throw new ValidationErrorException(ValidationErrorCodes.INVALID_STATE,
+                        "Service " + service.getName() + " is not in valid state to be deactivated: "
+                                + service.getState());
+            }
         }
     }
 }
