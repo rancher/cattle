78f68fce0c52291316cb67287446fec42782e090
kingsmiler <kingsmiler@163.com>
2016-02-24 20:06:10 +0800

    update docker-resources pom and reformat

--- docker/docker-resources/src/test/java/org/amc/docker/DockerResourcesApplicationTests.java
+++ docker/docker-resources/src/test/java/org/amc/docker/DockerResourcesApplicationTests.java
@@ -2,17 +2,17 @@ package org.amc.docker;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DockerResourcesApplication.class)
 @WebAppConfiguration
 public class DockerResourcesApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
