bfe96e9f36df8ad430e2742fea6e676620ee1833
Darren Shepherd <darren@rancher.com>
2016-03-07 15:19:34 -0700

    Make rancher-compose-executor and GMS use localhost for CATTLE_URL

--- code/implementation/docker/machine/src/main/java/io/cattle/platform/docker/machine/launch/ComposeExecutorLauncher.java
+++ code/implementation/docker/machine/src/main/java/io/cattle/platform/docker/machine/launch/ComposeExecutorLauncher.java
@@ -33,7 +33,7 @@ public class ComposeExecutorLauncher extends GenericServiceLauncher implements I
         Credential cred = getCredential();
         env.put("CATTLE_ACCESS_KEY", cred.getPublicValue());
         env.put("CATTLE_SECRET_KEY", cred.getSecretValue());
-        env.put("CATTLE_URL", ServerContext.getHostApiBaseUrl(BaseProtocol.HTTP));
+        env.put("CATTLE_URL", ServerContext.getLocalhostUrl(BaseProtocol.HTTP));
     }
 
     @Override
