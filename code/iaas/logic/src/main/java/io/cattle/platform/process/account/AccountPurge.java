0b3e53d0fe94a4c31953529d1f50a87b9075e962
Darren Shepherd <darren@rancher.com>
2016-03-08 02:19:46 -0700

    Delete certificates on account delete

--- code/iaas/logic/src/main/java/io/cattle/platform/process/account/AccountPurge.java
+++ code/iaas/logic/src/main/java/io/cattle/platform/process/account/AccountPurge.java
@@ -5,6 +5,7 @@ import io.cattle.platform.core.dao.AccountDao;
 import io.cattle.platform.core.dao.InstanceDao;
 import io.cattle.platform.core.model.Account;
 import io.cattle.platform.core.model.Agent;
+import io.cattle.platform.core.model.Certificate;
 import io.cattle.platform.core.model.Credential;
 import io.cattle.platform.core.model.Environment;
 import io.cattle.platform.core.model.Host;
@@ -34,6 +35,14 @@ public class AccountPurge extends AbstractDefaultProcessHandler {
     @Override
     public HandlerResult handle(ProcessState state, ProcessInstance process) {
         Account account = (Account) state.getResource();
+
+        for (Certificate cert : getObjectManager().children(account, Certificate.class)) {
+            if (cert.getRemoved() != null) {
+                continue;
+            }
+
+            deactivateThenRemove(cert, state.getData());
+        }
 
         for (Credential cred : getObjectManager().children(account, Credential.class)) {
             if (cred.getRemoved() != null) {
