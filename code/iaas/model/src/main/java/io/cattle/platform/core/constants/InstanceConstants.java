9f4e1813fc3ab556c86c6d989e7bf64476ddbb89
Alena Prokharchyk <alena@rancher.com>
2016-06-09 19:31:47 -0700

    Default logConfig addon "config" field

--- code/iaas/model/src/main/java/io/cattle/platform/core/constants/InstanceConstants.java
+++ code/iaas/model/src/main/java/io/cattle/platform/core/constants/InstanceConstants.java
@@ -54,6 +54,7 @@ public class InstanceConstants {
     public static final String FIELD_HOST_ID = "hostId";
     public static final String FIELD_DNS_INTERNAL = "dnsInternal";
     public static final String FIELD_DNS_SEARCH_INTERNAL = "dnsSearchInternal";
+    public static final String FIELD_LOG_CONFIG = "logConfig";
 
     public static final String PROCESS_DATA_NO_OP = "containerNoOpEvent";
 
