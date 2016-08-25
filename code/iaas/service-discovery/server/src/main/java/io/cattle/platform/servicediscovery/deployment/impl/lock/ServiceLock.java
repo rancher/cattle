1f0fb8ca0938ca25d340a8525833d501ab285b7d
Darren Shepherd <darren@rancher.com>
2016-08-25 14:21:10 -0700

    Fix tanglings of service.update and service.endpoint.update events

--- code/iaas/service-discovery/server/src/main/java/io/cattle/platform/servicediscovery/deployment/impl/lock/ServiceLock.java
+++ code/iaas/service-discovery/server/src/main/java/io/cattle/platform/servicediscovery/deployment/impl/lock/ServiceLock.java
@@ -1,9 +1,9 @@
 package io.cattle.platform.servicediscovery.deployment.impl.lock;
 
 import io.cattle.platform.core.model.Service;
-import io.cattle.platform.lock.definition.AbstractBlockingLockDefintion;
+import io.cattle.platform.lock.definition.AbstractLockDefinition;
 
-public class ServiceLock extends AbstractBlockingLockDefintion {
+public class ServiceLock extends AbstractLockDefinition {
 
     public ServiceLock(Service service) {
         super("SERVICE." + service.getId());
