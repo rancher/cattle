294fb099c015c488cecfae9e5e4e4ab104ea74ca
Darren Shepherd <darren@rancher.com>
2016-10-03 13:21:13 -0700

    Remove unused RetryTimeoutService.timeout() method

--- code/framework/async/src/main/java/io/cattle/platform/async/retry/RetryTimeoutService.java
+++ code/framework/async/src/main/java/io/cattle/platform/async/retry/RetryTimeoutService.java
@@ -1,10 +1,6 @@
 package io.cattle.platform.async.retry;
 
-import java.util.concurrent.Future;
-
 public interface RetryTimeoutService {
-
-    Object timeout(Future<?> future, long timeout);
 
     Object submit(Retry retry);
 
