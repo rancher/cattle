eeabfadcacc673f69aed0a4c91fedacfc82558ce
Darren Shepherd <darren@rancher.com>
2016-11-22 12:08:49 -0700

    Update host IP when changes

---
+++ code/iaas/agent-instance/src/main/java/io/cattle/platform/agent/instance/service/AgentMetadataService.java
@@ -0,0 +1,29 @@
+package io.cattle.platform.agent.instance.service;
+
+import io.cattle.platform.agent.instance.dao.AgentInstanceDao;
+import io.cattle.platform.configitem.request.ConfigUpdateRequest;
+import io.cattle.platform.configitem.version.ConfigItemStatusManager;
+import io.cattle.platform.core.model.Agent;
+import io.cattle.platform.core.util.SystemLabels;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+public class AgentMetadataService {
+
+    @Inject
+    AgentInstanceDao agentInstanceDao;
+    @Inject
+    ConfigItemStatusManager statusManager;
+
+    public void updateMetadata(Long accountId) {
+        List<Long> agentIds = agentInstanceDao.getAgentProviderIgnoreHealth(SystemLabels.LABEL_AGENT_SERVICE_METADATA, accountId);
+        for (long agentId : agentIds) {
+            ConfigUpdateRequest request = ConfigUpdateRequest.forResource(Agent.class, agentId);
+            request.addItem("metadata-answers");
+            statusManager.updateConfig(request);
+        }
+    }
+
+}
