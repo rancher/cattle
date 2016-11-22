17cc445312164514b0df7aaddab78b3a21568493
Darren Shepherd <darren@rancher.com>
2016-11-22 12:07:41 -0700

    Don't allow delete of rancher-agent

---
+++ code/iaas/api-logic/src/main/java/io/cattle/platform/iaas/api/filter/instance/InstanceAgentValidationFilter.java
@@ -0,0 +1,49 @@
+package io.cattle.platform.iaas.api.filter.instance;
+
+import io.cattle.platform.core.constants.InstanceConstants;
+import io.cattle.platform.core.model.Instance;
+import io.cattle.platform.iaas.api.filter.common.AbstractDefaultResourceManagerFilter;
+import io.cattle.platform.object.ObjectManager;
+import io.cattle.platform.object.util.DataAccessor;
+import io.github.ibuildthecloud.gdapi.exception.ClientVisibleException;
+import io.github.ibuildthecloud.gdapi.request.ApiRequest;
+import io.github.ibuildthecloud.gdapi.request.resource.ResourceManager;
+import io.github.ibuildthecloud.gdapi.util.ResponseCodes;
+import io.github.ibuildthecloud.gdapi.validation.ValidationErrorCodes;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+public class InstanceAgentValidationFilter extends AbstractDefaultResourceManagerFilter {
+
+    @Inject
+    ObjectManager objectManager;
+
+    @Override
+    public String[] getTypes() {
+        return new String[] {InstanceConstants.TYPE_CONTAINER};
+    }
+
+    @Override
+    public Class<?>[] getTypeClasses() {
+        return new Class<?>[] { Instance.class };
+    }
+
+    @Override
+    public Object delete(String type, String id, ApiRequest request, ResourceManager next) {
+        Object instance = objectManager.loadResource(type, id);
+        if (instance == null) {
+            return super.delete(type, id, request, next);
+        }
+
+        Map<String, Object> labels = DataAccessor.fieldMap(instance, InstanceConstants.FIELD_LABELS);
+        if ("rancher-agent".equals(labels.get("io.rancher.container.system"))) {
+            throw new ClientVisibleException(ResponseCodes.UNPROCESSABLE_ENTITY, ValidationErrorCodes.ACTION_NOT_AVAILABLE,
+                    "Can not delete rancher-agent", null);
+        }
+
+        return super.delete(type, id, request, next);
+    }
+
+}
