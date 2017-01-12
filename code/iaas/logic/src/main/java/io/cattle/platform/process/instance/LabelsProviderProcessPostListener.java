179101f2b8fc77241d01e712e814fd3f1398eb52
Darren Shepherd <darren@rancher.com>
2017-01-12 08:16:18 -0700

    Make labels provider required for K8s pods

--- code/iaas/logic/src/main/java/io/cattle/platform/process/instance/LabelsProviderProcessPostListener.java
+++
@@ -1,6 +0,0 @@
-package io.cattle.platform.process.instance;
-
-import io.cattle.platform.engine.handler.ProcessPostListener;
-
-public class LabelsProviderProcessPostListener extends LabelsProviderProcessHandler implements ProcessPostListener {
-}
--- code/iaas/logic/src/main/java/io/cattle/platform/process/instance/LabelsProviderProcessPreListener.java
+++
@@ -1,6 +0,0 @@
-package io.cattle.platform.process.instance;
-
-import io.cattle.platform.engine.handler.ProcessPreListener;
-
-public class LabelsProviderProcessPreListener extends LabelsProviderProcessHandler implements ProcessPreListener {
-}
