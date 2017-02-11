bba30c0c910714697ee7678d01d7f9c248d5d78c
Darren Shepherd <darren@rancher.com>
2017-02-10 20:03:57 -0700

    Don't send as much data in events

--- code/framework/eventing/src/main/java/io/cattle/platform/eventing/model/Event.java
+++ code/framework/eventing/src/main/java/io/cattle/platform/eventing/model/Event.java
@@ -3,6 +3,9 @@ package io.cattle.platform.eventing.model;
 import java.util.Date;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 public interface Event {
 
     public static final String TRANSITIONING_YES = "yes";
@@ -16,31 +19,42 @@ public interface Event {
 
     String getName();
 
+    @JsonInclude(Include.NON_EMPTY)
     String getReplyTo();
 
     String getResourceId();
 
     String getResourceType();
 
+    @JsonInclude(Include.NON_EMPTY)
     String[] getPreviousIds();
 
+    @JsonInclude(Include.NON_EMPTY)
     String[] getPreviousNames();
 
+    @JsonInclude(Include.NON_EMPTY)
     String getTransitioning();
 
+    @JsonInclude(Include.NON_EMPTY)
     Integer getTransitioningProgress();
 
+    @JsonInclude(Include.NON_EMPTY)
     String getTransitioningMessage();
 
+    @JsonInclude(Include.NON_EMPTY)
     String getTransitioningInternalMessage();
 
     Object getData();
 
+    @JsonInclude(Include.NON_EMPTY)
     Date getTime();
 
+    @JsonInclude(Include.NON_EMPTY)
     Long getTimeoutMillis();
 
+    @JsonInclude(Include.NON_EMPTY)
     String getPublisher();
 
+    @JsonInclude(Include.NON_EMPTY)
     Map<String, Object> getContext();
 }
