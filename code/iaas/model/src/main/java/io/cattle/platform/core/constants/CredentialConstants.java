d5c4fc7d0e7d0d8bef3c489dae597cb3af42c960
Darren Shepherd <darren@rancher.com>
2017-06-14 14:10:15 -0700

    Remove unused ssh key code

--- code/iaas/model/src/main/java/io/cattle/platform/core/constants/CredentialConstants.java
+++ code/iaas/model/src/main/java/io/cattle/platform/core/constants/CredentialConstants.java
@@ -12,7 +12,6 @@ public class CredentialConstants {
     public static final String KIND_API_KEY = "apiKey";
     public static final String KIND_PASSWORD = "password";
     public static final String KIND_AGENT_API_KEY = "agentApiKey";
-    public static final String KIND_SSH_KEY = "sshKey";
 
     public static final String LINK_PEM_FILE = "pem";
     public static final String LINK_CERTIFICATE = "certificate";
