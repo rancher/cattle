55bac8e1a392797ea1ed9174a4ae5b9b6fb42b7d
rajashree <rajashree.28m@gmail.com>
2017-07-26 17:48:48 -0700

    Remove externalId from call to refreshToken

--- code/iaas/auth-logic/src/main/java/io/cattle/platform/iaas/api/auth/integration/external/ExternalServiceAuthProvider.java
+++ code/iaas/auth-logic/src/main/java/io/cattle/platform/iaas/api/auth/integration/external/ExternalServiceAuthProvider.java
@@ -118,7 +118,7 @@ public class ExternalServiceAuthProvider {
         }
     }
 
-    public Token refreshToken(String accessToken, String externalId) {
+    public Token refreshToken(String accessToken) {
         //get the token from the auth service
         StringBuilder authUrl = new StringBuilder(ServiceAuthConstants.AUTH_SERVICE_URL.get());
         authUrl.append("/token");
@@ -127,7 +127,6 @@ public class ExternalServiceAuthProvider {
         try {
             Map<String, String> data = new HashMap<String, String>();
             data.put("accessToken", accessToken);
-            data.put("externalId", externalId);
             String jsonString = jsonMapper.writeValueAsString(data);
 
             Request temp = Request.Post(authUrl.toString()).addHeader(ServiceAuthConstants.ACCEPT, ServiceAuthConstants.APPLICATION_JSON)
@@ -279,13 +278,12 @@ public class ExternalServiceAuthProvider {
             return tokenUtil.getIdentities();
         }
         String jwt = null;
-        if (SecurityConstants.SECURITY.get()) {
-            if (!StringUtils.isBlank(accessToken) || account.getExternalId()!= null) {
+        if (SecurityConstants.SECURITY.get() && !StringUtils.isBlank(accessToken)) {
                 AuthToken authToken = authTokenDao.getTokenByAccountId(account.getId());
                 if (authToken == null) {
                     try {
                         //refresh token API.
-                        Token token = refreshToken(accessToken, account.getExternalId());
+                        Token token = refreshToken(accessToken);
                         if (token != null) {
                             jwt = ProjectConstants.AUTH_TYPE + token.getJwt();
                             authToken = authTokenDao.createToken(token.getJwt(), token.getAuthProvider(), account.getId(), account.getId());
@@ -300,7 +298,6 @@ public class ExternalServiceAuthProvider {
                     jwt = authToken.getKey();
                 }
             }
-        }
         if (StringUtils.isBlank(jwt)){
             return Collections.emptySet();
         }
