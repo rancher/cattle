61778451c5686144f57bc5377f7b899685c8d3d4
Prachi Damle <prachi@rancher.com>
2017-07-27 13:53:04 -0700

    Revert "Merge pull request #2820 from mrajashree/removeAD"
    This reverts commit c832ba697e910d97956ae4efaf0bd062313c1e1f, reversing
    changes made to a54b04bd2c184418c8222180540ec01b6fc0e2a8.

--- code/packaging/app-config/src/main/java/io/cattle/platform/app/ApiServerConfig.java
+++ code/packaging/app-config/src/main/java/io/cattle/platform/app/ApiServerConfig.java
@@ -66,6 +66,7 @@ import io.cattle.platform.iaas.api.auth.dao.impl.PasswordDaoImpl;
 import io.cattle.platform.iaas.api.auth.identity.Token;
 import io.cattle.platform.iaas.api.auth.integration.azure.AzureConfig;
 import io.cattle.platform.iaas.api.auth.integration.ldap.OpenLDAP.OpenLDAPConfig;
+import io.cattle.platform.iaas.api.auth.integration.ldap.ad.ADConfig;
 import io.cattle.platform.iaas.api.auth.integration.local.ChangeSecretActionHandler;
 import io.cattle.platform.iaas.api.auth.integration.local.LocalAuthConfig;
 import io.cattle.platform.iaas.api.auth.projects.Member;
@@ -419,6 +420,7 @@ public class ApiServerConfig {
                 Token.class,
                 Member.class,
                 Identity.class,
+                ADConfig.class,
                 OpenLDAPConfig.class,
                 LocalAuthConfig.class,
                 AzureConfig.class
