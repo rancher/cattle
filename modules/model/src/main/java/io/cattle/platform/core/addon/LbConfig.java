38af33f84c3b289ce60498936f43ae612ebf80d7
Alena Prokharchyk <alena@rancher.com>
2017-09-14 16:08:46 -0700

    Missing nullable=true attribute on LB

--- modules/model/src/main/java/io/cattle/platform/core/addon/LbConfig.java
+++ modules/model/src/main/java/io/cattle/platform/core/addon/LbConfig.java
@@ -5,7 +5,7 @@ import io.github.ibuildthecloud.gdapi.annotation.Type;
 
 import java.util.List;
 
-@Type(list = false)
+@Type(list = false, name = "lbConfig")
 public class LbConfig {
     String config;
     List<PortRule> portRules;
@@ -26,6 +26,7 @@ public class LbConfig {
     public LbConfig() {
     }
 
+    @Field(nullable = true)
     public String getConfig() {
         return config;
     }
@@ -42,7 +43,7 @@ public class LbConfig {
         this.portRules = portRules;
     }
 
-    @Field(typeString = "array[reference[certificate]]")
+    @Field(typeString = "array[reference[certificate]]", nullable = true)
     public List<Long> getCertificateIds() {
         return certificateIds;
     }
@@ -51,7 +52,7 @@ public class LbConfig {
         this.certificateIds = certificateIds;
     }
 
-    @Field(typeString = "reference[certificate]")
+    @Field(typeString = "reference[certificate]", nullable = true)
     public Long getDefaultCertificateId() {
         return defaultCertificateId;
     }
@@ -60,12 +61,62 @@ public class LbConfig {
         this.defaultCertificateId = defaultCertificateId;
     }
 
+    @Field(nullable = true)
     public LoadBalancerCookieStickinessPolicy getStickinessPolicy() {
         return stickinessPolicy;
     }
 
     public void setStickinessPolicy(LoadBalancerCookieStickinessPolicy stickinessPolicy) {
         this.stickinessPolicy = stickinessPolicy;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((certificateIds == null) ? 0 : certificateIds.hashCode());
+        result = prime * result + ((config == null) ? 0 : config.hashCode());
+        result = prime * result + ((defaultCertificateId == null) ? 0 : defaultCertificateId.hashCode());
+        result = prime * result + ((portRules == null) ? 0 : portRules.hashCode());
+        result = prime * result + ((stickinessPolicy == null) ? 0 : stickinessPolicy.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        LbConfig other = (LbConfig) obj;
+        if (certificateIds == null) {
+            if (other.certificateIds != null)
+                return false;
+        } else if (!certificateIds.equals(other.certificateIds))
+            return false;
+        if (config == null) {
+            if (other.config != null)
+                return false;
+        } else if (!config.equals(other.config))
+            return false;
+        if (defaultCertificateId == null) {
+            if (other.defaultCertificateId != null)
+                return false;
+        } else if (!defaultCertificateId.equals(other.defaultCertificateId))
+            return false;
+        if (portRules == null) {
+            if (other.portRules != null)
+                return false;
+        } else if (!portRules.equals(other.portRules))
+            return false;
+        if (stickinessPolicy == null) {
+            if (other.stickinessPolicy != null)
+                return false;
+        } else if (!stickinessPolicy.equals(other.stickinessPolicy))
+            return false;
+        return true;
     }
 
 }
--- modules/model/src/main/java/io/cattle/platform/core/addon/LoadBalancerCookieStickinessPolicy.java
+++ modules/model/src/main/java/io/cattle/platform/core/addon/LoadBalancerCookieStickinessPolicy.java
@@ -81,4 +81,62 @@ public class LoadBalancerCookieStickinessPolicy {
     public void setCookie(String cookie) {
         this.cookie = cookie;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cookie == null) ? 0 : cookie.hashCode());
+        result = prime * result + ((domain == null) ? 0 : domain.hashCode());
+        result = prime * result + ((indirect == null) ? 0 : indirect.hashCode());
+        result = prime * result + ((mode == null) ? 0 : mode.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((nocache == null) ? 0 : nocache.hashCode());
+        result = prime * result + ((postonly == null) ? 0 : postonly.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        LoadBalancerCookieStickinessPolicy other = (LoadBalancerCookieStickinessPolicy) obj;
+        if (cookie == null) {
+            if (other.cookie != null)
+                return false;
+        } else if (!cookie.equals(other.cookie))
+            return false;
+        if (domain == null) {
+            if (other.domain != null)
+                return false;
+        } else if (!domain.equals(other.domain))
+            return false;
+        if (indirect == null) {
+            if (other.indirect != null)
+                return false;
+        } else if (!indirect.equals(other.indirect))
+            return false;
+        if (!mode.toString().equals(other.mode.toString()))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (nocache == null) {
+            if (other.nocache != null)
+                return false;
+        } else if (!nocache.equals(other.nocache))
+            return false;
+        if (postonly == null) {
+            if (other.postonly != null)
+                return false;
+        } else if (!postonly.equals(other.postonly))
+            return false;
+        return true;
+    }
 }
