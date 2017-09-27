64511fd2b31078e84ecd96d0c84b567287f60f5e
Alena Prokharchyk <alena@rancher.com>
2017-09-27 15:17:45 -0700

    Evaluate registry uniqueness on per cluster basis

--- modules/caas/api/src/main/java/io/cattle/platform/api/registry/RegistryServerAddressFilter.java
+++ modules/caas/api/src/main/java/io/cattle/platform/api/registry/RegistryServerAddressFilter.java
@@ -3,10 +3,12 @@ package io.cattle.platform.api.registry;
 import io.cattle.platform.api.auth.Policy;
 import io.cattle.platform.core.constants.CommonStatesConstants;
 import io.cattle.platform.core.constants.StoragePoolConstants;
+import io.cattle.platform.core.model.Account;
 import io.cattle.platform.core.model.StoragePool;
 import io.cattle.platform.object.ObjectManager;
 import io.cattle.platform.object.meta.ObjectMetaDataManager;
 import io.cattle.platform.util.type.CollectionUtils;
+
 import io.github.ibuildthecloud.gdapi.context.ApiContext;
 import io.github.ibuildthecloud.gdapi.exception.ClientVisibleException;
 import io.github.ibuildthecloud.gdapi.request.ApiRequest;
@@ -31,9 +33,11 @@ public class RegistryServerAddressFilter extends AbstractValidationFilter {
         Map<String, Object> requestObject = CollectionUtils.toMap(request.getRequestObject());
         String serverAddress = (String) requestObject.get(StoragePoolConstants.SERVER_ADDRESS);
         long accountId = ((Policy) ApiContext.getContext().getPolicy()).getAccountId();
+        Account account = objectManager.loadResource(Account.class, accountId);
         List<StoragePool> registries = objectManager.find(StoragePool.class,
                 ObjectMetaDataManager.KIND_FIELD, StoragePoolConstants.KIND_REGISTRY,
                 ObjectMetaDataManager.ACCOUNT_FIELD, accountId,
+                ObjectMetaDataManager.CLUSTER_FIELD, account.getClusterId(),
                 ObjectMetaDataManager.REMOVED_FIELD, null);
         for (StoragePool registry: registries){
             if (!CommonStatesConstants.REMOVED.equalsIgnoreCase(registry.getState())) {
