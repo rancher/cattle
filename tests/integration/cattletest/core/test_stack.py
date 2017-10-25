f4ab41d9cd02e16dc5d88b23cb0baf6309d37375
kinarashah <kinarahs@usc.edu>
2017-10-25 15:59:22 -0700

    validate stack name on edit
    validate stack name on edit
    
    fix NPE
    
    tests

--- tests/integration/cattletest/core/test_stack.py
+++ tests/integration/cattletest/core/test_stack.py
@@ -1,4 +1,5 @@
 from common_fixtures import *  # NOQA
+from cattle import ApiError
 
 binding_input = {
     "services":
@@ -25,3 +26,27 @@ def test_create_stack_binding(client):
     assert "service_1" in env.binding.services
     assert env.binding.services.service_1.labels == {"label_1": "value_1"}
     assert env.binding.services.service_1.ports == []
+
+
+def test_validate_stack_name(client):
+    # stack_name starting/ending in hyphen
+    for stack_name in ("-"+random_str(), random_str()+"-"):
+        with pytest.raises(ApiError) as e:
+            client.create_stack(name=stack_name)
+        assert e.value.error.status == 422
+        e.value.error.code == 'InvalidCharacters'
+
+
+def test_duplicate_stack_name(client):
+    env = _create_stack(client)
+    env2 = _create_stack(client)
+    with pytest.raises(ApiError) as e:
+        client.create_stack(name=env.name)
+    assert e.value.error.status == 422
+    assert e.value.error.code == 'NotUnique'
+    assert e.value.error.fieldName == 'name'
+    with pytest.raises(ApiError) as e:
+        client.update(env, name=env2.name)
+    assert e.value.error.status == 422
+    assert e.value.error.code == 'NotUnique'
+    assert e.value.error.fieldName == 'name'
