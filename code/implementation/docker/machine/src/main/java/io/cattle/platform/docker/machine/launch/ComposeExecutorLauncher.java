aedc0ace721d97f718e82112ab721fe8e8d137c0
Daishan Peng <daishan@rancher.com>
2019-01-08 18:45:54 -0700

    add timeout option to rce

--- code/implementation/docker/machine/src/main/java/io/cattle/platform/docker/machine/launch/ComposeExecutorLauncher.java
+++ code/implementation/docker/machine/src/main/java/io/cattle/platform/docker/machine/launch/ComposeExecutorLauncher.java
@@ -9,6 +9,8 @@ import io.cattle.platform.server.context.ServerContext.BaseProtocol;
 import io.cattle.platform.service.launcher.GenericServiceLauncher;
 import io.cattle.platform.util.type.InitializationTask;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
@@ -20,6 +22,7 @@ import com.netflix.config.DynamicStringProperty;
 public class ComposeExecutorLauncher extends GenericServiceLauncher implements InitializationTask {
 
     private static final DynamicStringProperty COMPOSE_EXECUTOR_BINARY = ArchaiusUtil.getString("compose.executor.service.executable");
+    private static final DynamicStringProperty COMPOSE_EXECUTOR_CLIENT_TIMEOUT = ArchaiusUtil.getString("compose.executor.service.executable.timeout");
     private static final DynamicBooleanProperty LAUNCH_COMPOSE_EXECUTOR = ArchaiusUtil.getBoolean("compose.executor.execute");
 
     @Inject
@@ -41,6 +44,7 @@ public class ComposeExecutorLauncher extends GenericServiceLauncher implements I
         env.put("CATTLE_ACCESS_KEY", cred.getPublicValue());
         env.put("CATTLE_SECRET_KEY", cred.getSecretValue());
         env.put("CATTLE_URL", ServerContext.getLocalhostUrl(BaseProtocol.HTTP));
+        env.put("RANCHER_CLIENT_TIMEOUT", COMPOSE_EXECUTOR_CLIENT_TIMEOUT.get());
     }
 
     @Override
@@ -51,6 +55,13 @@ public class ComposeExecutorLauncher extends GenericServiceLauncher implements I
     @Override
     protected boolean isReady() {
         return true;
+    }
+    
+    @Override
+    protected List<DynamicStringProperty> getReloadSettings() {
+        List<DynamicStringProperty> list = new ArrayList<>();
+        list.add(COMPOSE_EXECUTOR_CLIENT_TIMEOUT);
+        return list;
     }
 
 }
